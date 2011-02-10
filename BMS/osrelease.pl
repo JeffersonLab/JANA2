b5ae9cab77049ccd3a5d5906a79b46e1752cc000
David Lawrence <davidl@jlab.org>
2011-02-10 21:54:55 +0000

    Fix osrelease.pl to give something more reasonable than 'unknown' on Ubuntu systems for the processor type.
    git-svn-id: https://phys12svn.jlab.org/repos/JANA@736 c5ed4466-e916-0410-8347-b3263e9c103d

--- BMS/osrelease.pl
+++ BMS/osrelease.pl
@@ -42,49 +42,52 @@ $uname = `uname`;
 chomp $uname;
 if ($uname eq 'Linux') {
     if (-e '/etc/fedora-release') {
-	$release_string = `cat /etc/fedora-release`;
-	if ($release_string =~ /^Fedora release 11.*/) {
-	    $release = '_Fedora11';
-	} elsif ($release_string =~ /^Fedora release 10.*/) {
-	    $release = '_Fedora10';
-	} elsif ($release_string =~ /^Fedora release 9.*/) {
-	    $release = '_Fedora9';
-	} elsif ($release_string =~ /^Fedora release 8.*/) {
-	    $release = '_Fedora8';
-	} elsif ($release_string =~ /^Fedora release 7.*/) {
-	    $release = '_Fedora7';
-	} elsif ($release_string =~ /^Fedora Core release 6.*/) {
-	    $release = '_FC6';
-	} else {
-	    print STDERR "unrecognized Fedora release\n";
-	    $release = '_Fedora';
-	}
+		$release_string = `cat /etc/fedora-release`;
+		if ($release_string =~ /^Fedora release 11.*/) {
+			$release = '_Fedora11';
+		} elsif ($release_string =~ /^Fedora release 10.*/) {
+			$release = '_Fedora10';
+		} elsif ($release_string =~ /^Fedora release 9.*/) {
+			$release = '_Fedora9';
+		} elsif ($release_string =~ /^Fedora release 8.*/) {
+			$release = '_Fedora8';
+		} elsif ($release_string =~ /^Fedora release 7.*/) {
+			$release = '_Fedora7';
+		} elsif ($release_string =~ /^Fedora Core release 6.*/) {
+			$release = '_FC6';
+		} else {
+			print STDERR "unrecognized Fedora release\n";
+			$release = '_Fedora';
+		}
     } elsif (-e '/etc/redhat-release') {
-	$release_string = `cat /etc/redhat-release`;
-	if ($release_string =~ /^Red Hat Enterprise Linux WS release 3.*/) {
-	    $release = '_RHEL3';
-	} elsif ($release_string =~ /^Red Hat Enterprise Linux WS release 4.*/) {
-	    $release = '_RHEL4';
-	} elsif ($release_string =~ /^Red Hat Enterprise Linux Client release 5.*/) {
-	    $release = '_RHEL5';
-	} elsif ($release_string =~ /^CentOS release 4.*/ ){
-	    $release = '_CentOS4';
-	} elsif ($release_string =~ /^CentOS release 5.*/ ){
-	    $release = '_CentOS5';
-	} else {
-	    print STDERR "unrecognized Red Hat release\n";
-	    $release = '_RH';
-	}
+		$release_string = `cat /etc/redhat-release`;
+		if ($release_string =~ /^Red Hat Enterprise Linux WS release 3.*/) {
+			$release = '_RHEL3';
+		} elsif ($release_string =~ /^Red Hat Enterprise Linux WS release 4.*/) {
+			$release = '_RHEL4';
+		} elsif ($release_string =~ /^Red Hat Enterprise Linux Client release 5.*/) {
+			$release = '_RHEL5';
+		} elsif ($release_string =~ /^CentOS release 4.*/ ){
+			$release = '_CentOS4';
+		} elsif ($release_string =~ /^CentOS release 5.*/ ){
+			$release = '_CentOS5';
+		} else {
+			print STDERR "unrecognized Red Hat release\n";
+			$release = '_RH';
+		}
     } elsif (-e '/etc/lsb-release') { # Ubuntu
     	$distrib_id = `cat /etc/lsb-release | grep DISTRIB_ID`;
-	$distrib_id =~ s/DISTRIB_ID=//;
+		$distrib_id =~ s/DISTRIB_ID=//;
     	$distrib_release = `cat /etc/lsb-release | grep DISTRIB_RELEASE`;
-	$distrib_release =~ s/DISTRIB_RELEASE=//;
-	chomp $distrib_id;
-	chomp $distrib_release;
-	$release = "_${distrib_id}${distrib_release}";	
+		$distrib_release =~ s/DISTRIB_RELEASE=//;
+		chomp $distrib_id;
+		chomp $distrib_release;
+		$release = "_${distrib_id}${distrib_release}";
+		if( $processor eq "unknown" ){
+			$processor=$ENV{"MACHTYPE"};
+		}
     } else {
-	$release = '';
+		$release = '';
     }
 } elsif ($uname eq 'SunOS') {
 	$release = '_' . `uname -r`;
