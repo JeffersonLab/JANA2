1fb7315a0ebc42d3944105d570a56ab30c4cbccd
David Lawrence <davidl@jlab.org>
2013-01-02 17:29:44 +0000

    Updated unit test for JResourceManager. Tests both option 1 and option 2 style accessing. Also does multiple Gets to check that file is not downloaded twice, but testing does not check this directly. (It only checks that a second call succeeds).
    git-svn-id: https://phys12svn.jlab.org/repos/JANA@1152 c5ed4466-e916-0410-8347-b3263e9c103d

--- src/unit_tests/resource_test/JResourceManager_test.cc
+++ src/unit_tests/resource_test/JResourceManager_test.cc
@@ -20,7 +20,11 @@ using namespace jana;
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
-unsigned int TestResourceManager(void);
+int NARG;
+char **ARGV;
+
+unsigned int TestResourceManager_Option1(void);
+unsigned int TestResourceManager_Option2(void);
 
 //------------------
 // main
@@ -30,15 +34,11 @@ int main(int narg, char *argv[])
 	cout<<endl;
 	jout<<"----- starting JResourceManager unit test ------"<<endl;
 
-	// Create a JApplication object
-	JApplication *app = new JApplication(narg, argv);
+	// Record command line args for later use
+	NARG = narg;
+	ARGV = argv;
 
 	int result = Catch::Main( narg, argv );
-
-	// Cleanup
-	delete app;
-
-	jout<<"----- finished JResourceManager unit test ------"<<endl;
 
 	return result;
 }
@@ -46,15 +46,119 @@ int main(int narg, char *argv[])
 //------------------
 // TEST_CASE
 //------------------
-TEST_CASE("resource/get", "Gets a remote resource using JResourceManager through JApplication")
+TEST_CASE("resource/get option 1", "Gets a remote resource using JResourceManager through JApplication")
 {
-	REQUIRE( TestResourceManager() == 175951 );
+	// Create JApplication for use with first 3 tests
+	JApplication *app = new JApplication(NARG, ARGV);
+
+	// First time should download
+	jout << "------------------ Option 1 Test 1 (download)-------------------" << endl;
+	REQUIRE( TestResourceManager_Option1() == 175951 );
+
+	// Second time uses already downloaded file
+	jout << "------------------ Option 1 Test 2 (no download)-------------------" << endl;
+	REQUIRE( TestResourceManager_Option1() == 175951 );
+
+	// Check that option 2 works with file downloaded using option 1
+	jout << "------------------ Option 1 Test 3 (no download)-------------------" << endl;
+	REQUIRE( TestResourceManager_Option2() == 175951 );
+
+	// Clean up temporary directories
+	jout<<"Removing temporary directories/files ..."<<endl;
+	system("rm -rf tmp_calib tmp_resources");
+
+	delete app;
+}
+//------------------
+// TEST_CASE
+//------------------
+TEST_CASE("resource/get option 2", "Gets a remote resource using JResourceManager through JApplication")
+{
+	// Create JApplication for use with first 3 tests
+	JApplication *app = new JApplication(NARG, ARGV);
+
+	// First time should download
+	jout << "------------------ Option 2 Test 1 (download)-------------------" << endl;
+	REQUIRE( TestResourceManager_Option2() == 175951 );
+	
+	// Second time uses already downloaded file
+	jout << "------------------ Option 2 Test 2 (no download)-------------------" << endl;
+	REQUIRE( TestResourceManager_Option2() == 175951 );
+
+	// Clean up temporary directories
+	jout<<"Removing temporary directories/files ..."<<endl;
+	system("rm -rf tmp_calib tmp_resources");
+
+	delete app;
 }
 
 //------------------
-// TestResourceManager
+// TestResourceManager_Option1
 //------------------
-unsigned int TestResourceManager(void)
+unsigned int TestResourceManager_Option1(void)
+{
+	// Create a temporary calibration directory
+	char cwd_buff[1024];
+	getcwd(cwd_buff, 1024);
+	string cwd(cwd_buff);
+	string calibdir = cwd + "/tmp_calib";
+	jout<<"Making temporary calibration directory: "<< calibdir <<endl;
+	mkdir(calibdir.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
+	
+	// Create an empty info.xml file in resources directory
+	// to avoid warning from JCalibrationFile
+	string info_xml = calibdir + "/info.xml";
+	ofstream tmp_ofs(info_xml.c_str());
+	tmp_ofs.close();
+	
+	// Make subdirectory "test"
+	mkdir((calibdir+"/test").c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
+	
+	// Create a resource description file in the new directory
+	string fname = "test/fieldmap";
+	string fname_fullpath = calibdir + "/" + fname;
+	string URL_base = "https://halldsvn.jlab.org/repos/tags/calib-2011-07-31";
+	string path = "Magnets/Solenoid/solenoid_1500_poisson_20090814_01";
+	jout<<"Writing "<<fname<<" to calibration directory ..." <<endl;
+	ofstream ofs(fname_fullpath.c_str());
+	ofs<<"# Auto-generated, temporary test file" << endl;
+	ofs<<"URL_base " << URL_base << endl;
+	ofs<<"path " << path << endl;
+	ofs<<endl;
+	ofs.close();
+	
+	// Set the JANA_CALIB_URL environment variable to point to
+	// the temporary directory.
+	char putenv_str[256];
+	sprintf(putenv_str, "JANA_CALIB_URL=file://%s", calibdir.c_str());
+	putenv(putenv_str);
+	
+	// Set the resource directory name and set the
+	// JANA:RESOURCE_DIR configuration parameter to
+	// point to it
+	string resource_dir = cwd + "/tmp_resources";
+	jout<<"Setting resources directory to: "<<resource_dir<<endl;
+	gPARMS->SetParameter("JANA:RESOURCE_DIR", resource_dir);
+
+	// Have JApplication create a JCalibration object
+	japp->GetJCalibration(1);
+	
+	// Have JApplication create a JResourceManager object
+	JResourceManager *jrm = japp->GetJResourceManager(1);
+
+	// Get the resource
+	vector< vector<float> > Bmap;
+	jout << "Reading field map ..." << endl;
+	jrm->Get("test/fieldmap", Bmap);
+	jout<<Bmap.size()<<" entries found" << endl;
+	
+	return Bmap.size();
+}
+
+//------------------
+// TestResourceManager_Option2
+//------------------
+unsigned int TestResourceManager_Option2(void)
 {
 	// Create a temporary calibration directory
 	char cwd_buff[1024];
@@ -109,9 +213,5 @@ unsigned int TestResourceManager(void)
 	jrm->Get("test/fieldmap", Bmap);
 	jout<<Bmap.size()<<" entries found" << endl;
 	
-	// Clean up temporary directories
-	jout<<"Removing temporary directories/files ..."<<endl;
-	system("rm -rf tmp_calib tmp_resources");
-
 	return Bmap.size();
 }
