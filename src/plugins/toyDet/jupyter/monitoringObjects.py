d7d57a84397a91243576efefcdf70ae8737986fc
pooser <pooser@jlab.org>
2019-09-19 16:27:41 -0400

    include occupancy plot and improve modularization

--- src/plugins/toyDet/jupyter/monitoringObjects.py
+++ src/plugins/toyDet/jupyter/monitoringObjects.py
@@ -11,8 +11,44 @@ from ipywidgets import HBox
 numChans = 80
 
 
-class MonitoringFigure:
-    """Class to create figure and subplots based on user input"""
+class OccupancyFig:
+    """Class to create figure for occupancy plots"""
+
+    # define figure and plot attributes
+    def __init__(self):
+        self.fig = plt.figure()
+        self.fig.set_size_inches(18.5, 10.5, forward=True)
+
+
+class OccupancyPlot:
+    """Class to plot streaming occupancy data"""
+
+    def __init__(self, dataDict, thresh, fig, hitCounter):
+        self.hitThresh  = thresh
+        self.fig = fig
+        self.hitCounter = hitCounter
+        self.chanList = np.arange(1, numChans + 1, 1)
+        for chan in range(1, numChans + 1):
+            if len(np.where(dataDict['adcSamplesChan_%d' % chan] > self.hitThresh)[0]) != 0:
+                self.hitCounter[chan-1] += 1
+            # remove event data after being published
+            dataDict.pop('adcSamplesChan_%s' % str(chan), None)
+            dataDict.pop('tdcSamplesChan_%s' % str(chan), None)
+        plt.bar(self.chanList, self.hitCounter, align='center')
+        plt.xlabel('Channel Number')
+        plt.ylabel('Number of ADC Hits > %d Channels' % self.hitThresh)
+        plt.title('ADC Occupancy')
+        set_matplotlib_close(False)
+        display(self.fig)
+        clear_output(wait = False)
+        plt.pause(0.05)
+
+    def get_hit_cntr(self):
+        return self.hitCounter
+
+
+class WaveFormFig:
+    """Class to create a figure and subplots based on user input for viewing waveforms"""
 
     # define figure and plot attributes
     def __init__(self, rows, cols):
@@ -20,6 +56,9 @@ class MonitoringFigure:
         self.numCols = cols
         self.fig, self.axs = plt.subplots(self.numRows, self.numCols)
         self.fig.set_size_inches(18.5, 10.5, forward=True)
+        # channel list, ascending and non-repeating
+        self.chans = random.sample(range(1, numChans + 1), self.numRows * self.numCols)
+        self.chans.sort()
 
     def get_num_rows(self):
         return self.numRows
@@ -33,38 +72,39 @@ class MonitoringFigure:
     def get_axs_obj(self):
         return self.axs
 
+    def get_chan_list(self):
+        return self.chans
 
-class MonitoringPlots:
+
+class WaveFormPlot:
     """Class to plot streaming ADC vs. TDC data"""
 
     # instance variables unique to each instance
-    def __init__(self, dataDict, thresh, rows, cols, fig, axs):
+    def __init__(self, dataDict, thresh, rows, cols, fig, axs, chans):
         # figure parameters
         self.hitThresh = thresh
         self.numRows = rows
         self.numCols = cols
         self.fig = fig
         self.axs = axs
+        self.chans = chans
         # lists for colors and markers
         self.cl = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',
                    'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
         self.ml = ['o', '^', 's', 'p', 'P', '*', 'X', 'd']
-        # random channel list, ascending and non-repeating
-        self.rcl = random.sample(range(1, numChans + 1), self.numRows * self.numCols)
-        self.rcl.sort()
         # event number list, index counter
         self.enl = []
         self.ic = 0
         for row in range(self.numRows):
             for column in range(self.numCols):
                 self.axs[row, column].cla()
-                self.axs[row, column].plot(dataDict['tdcSamplesChan_%d' % self.rcl[self.ic]],
-                                           dataDict['adcSamplesChan_%d' % self.rcl[self.ic]],
+                self.axs[row, column].plot(dataDict['tdcSamplesChan_%d' % self.chans[self.ic]],
+                                           dataDict['adcSamplesChan_%d' % self.chans[self.ic]],
                                            color=self.cl[self.ic % len(self.cl)],
                                            marker=self.ml[self.ic % len(self.ml)],
-                                           ls='', label='Channel %d' % self.rcl[self.ic])
-                hitLoc = np.where(dataDict['adcSamplesChan_%d' % self.rcl[self.ic]] > 100)[0] + \
-                         np.min(dataDict['tdcSamplesChan_%d' % self.rcl[self.ic]])
+                                           ls='', label='Channel %d' % self.chans[self.ic])
+                hitLoc = np.where(dataDict['adcSamplesChan_%d' % self.chans[self.ic]] > self.hitThresh)[0] + \
+                         np.min(dataDict['tdcSamplesChan_%d' % self.chans[self.ic]])
                 if len(hitLoc) != 0: self.axs[row, column].set_xlim(np.min(hitLoc) - 10, np.max(hitLoc) + 20)
                 self.axs[row, column].set_ylim(0, 1024)
                 if column == 0:
--- src/plugins/toyDet/jupyter/streamSubscriber.py
+++ src/plugins/toyDet/jupyter/streamSubscriber.py
@@ -1,33 +1,46 @@
 #!/usr/bin/python3.6
 
-import zmq
+import zmq, time
+import numpy as np
 from indraMessage import IndraMessage
-from monitoringObjects import MonitoringFigure, MonitoringPlots
+from monitoringObjects import *
+
+# define global variables
+numChans = 80
 
 
 class StreamSubscriber:
     """Class to handle INDRA Streaming"""
 
-    def __init__(self, port, nrows, ncols, thresh):
+    # def __init__(self, port, nrows, ncols, thresh):
+    # def __init__(self, port, thresh):
+    def __init__(self, type = 'occ', port = 5557, thresh = 100, nrows = 2, ncols = 2):
+        self.plotType = type
         self.subPort = port
+        self.hitThresh = thresh
         self.numRows = nrows
         self.numCols = ncols
-        self.hitThresh = thresh
         self.subContext = zmq.Context()
         self.subscriber = self.subContext.socket(zmq.SUB)
         self.subscriber.setsockopt(zmq.SUBSCRIBE, b'')
         self.subscriber.connect('tcp://127.0.0.1:%d' % self.subPort)
         print('\nSubscribing to JANA ZMQ Messages on socket tcp://127.0.0.1:%d\n' % self.subPort)
         # instantiate the monitoring figure class
-        self.monFig = MonitoringFigure(self.numRows, self.numCols)
+        if self.plotType == 'occ':
+            self.monFig = OccupancyFig()
+            self.hitCounter = np.zeros(numChans)
+        if self.plotType == 'wf':
+            self.monFig = WaveFormFig(self.numRows, self.numCols)
         # receive zmq messages from jana publisher
         while True:
             # receive zmq packets from jana publisher
             self.janaMessage = self.subscriber.recv()
             # instantiate the indra message class
             self.jevent = IndraMessage(self.janaMessage)
-            # instantiate the monitoring plot class
-            self.monPlots = MonitoringPlots(self.jevent.dataDict, self.hitThresh,
-                                            self.monFig.get_num_rows(), self.monFig.get_num_cols(),
-                                            self.monFig.get_fig_obj(), self.monFig.get_axs_obj())
-            # self.figCanvas = self.monFig.get_fig_obj().canvas
+            # instantiate the monitoring plot classes
+            if self.plotType == 'occ':
+                self.monPlots = OccupancyPlot(self.jevent.dataDict, self.hitThresh, self.monFig, self.hitCounter)
+            if self.plotType == 'wf':
+                self.monPlots = WaveFormPlot(self.jevent.dataDict, self.hitThresh, self.monFig.get_num_rows(),
+                                             self.monFig.get_num_cols(), self.monFig.get_fig_obj(),
+                                             self.monFig.get_axs_obj(), self.monFig.get_chan_list())
