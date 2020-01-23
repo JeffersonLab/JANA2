f800306a0702e7463b6a2e90c7c5267a2e47da1b
Nathan Brei <nathan.w.brei@gmail.com>
2020-01-23 18:14:05 -0500

    Ensure JEventProcessors are always furnished with a JApplication
    If the user doesn't pass the JApplication pointer via a constructor,
    the JComponentManager will step in and set it before anyone can
    call JEventProcessor::Init.
    
    This removes one cause of segfaults, and reduces the amount of
    boilerplate needed to create a JEventSource. The user no longer needs to
    create a throwaway constructor for their JEventSource simply to pass
    the JApplication pointer to the parent constructor. It also supports
    the broader pattern of moving logic out of the constructor and into
    Init.

--- src/lib/JANA/Services/JComponentManager.cc
+++ src/lib/JANA/Services/JComponentManager.cc
@@ -47,6 +47,9 @@ void JComponentManager::add(JEventSource *event_source) {
 
 void JComponentManager::add(JEventProcessor *processor) {
     processor->SetPluginName(m_current_plugin_name);
+    if (processor->GetApplication() == nullptr) {
+        processor->SetJApplication(m_app);
+    }
     m_evt_procs.push_back(processor);
 }
 
