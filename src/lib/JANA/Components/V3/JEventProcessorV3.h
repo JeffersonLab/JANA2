452447e71c5ac323252717ab967a62617cf1c86c
Nathan Brei <nbrei@jlab.org>
2020-02-08 01:32:22 -0500

    Work in progress: V3 event source and processor

--- src/lib/JANA/Components/V3/JEventProcessorV3.h
+++ src/lib/JANA/Components/V3/JEventProcessorV3.h
@@ -5,10 +5,115 @@
 #ifndef JANA2_JEVENTPROCESSORV3_H
 #define JANA2_JEVENTPROCESSORV3_H
 
+#include <JANA/Components/JAbstractEventProcessor.h>
+#include <JANA/JException.h>
+#include <JANA/JEvent.h>
 
-class JEventProcessorV3 {
+class JEventProcessorV3 : public JAbstractEventProcessor {
+public:
 
+    // ----------------------------------
+    // User is supposed to override these
+    // ----------------------------------
+
+    virtual void Init(JApplication* app) {}
+
+    virtual void ProcessParallel(const JEvent&) {}
+
+    virtual void ProcessSequential(const JEvent&) {}
+
+    virtual void Finish() {}
+
+    // TODO: Improve me!
+    // Tell the user to call SetType(), SetGlobalLock()
+    virtual std::string GetType() const { return m_type_name; }
+
+
+    // ------------------------
+    // Used internally by JANA
+    // ------------------------
+
+    void DoInitialize() final {
+        try {
+            std::call_once(m_init_flag, &JEventProcessorV3::Init, this, this->mApplication);
+            m_status = Status::Opened;
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventProcessorV2::Open()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+    void DoMap(const std::shared_ptr<const JEvent>& e) final {
+        try {
+            ProcessParallel(*e);
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventProcessorV3::DoMap()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+    void DoReduce(const std::shared_ptr<const JEvent>& e) final {
+        try {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            ProcessSequential(*e);
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventProcessorV3::DoReduce()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+    void DoFinalize() final {
+        try {
+            std::call_once(m_finish_flag, &JEventProcessorV3::Finish, this);
+            m_status = Status::Finished;
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventProcessorV3::Finish()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+private:
+    std::once_flag m_init_flag;
+    std::once_flag m_finish_flag;
+    std::mutex m_mutex;
 };
+
 
 
 #endif //JANA2_JEVENTPROCESSORV3_H
--- src/lib/JANA/Components/V3/JEventSourceV3.h
+++ src/lib/JANA/Components/V3/JEventSourceV3.h
@@ -5,10 +5,122 @@
 #ifndef JANA2_JEVENTSOURCEV3_H
 #define JANA2_JEVENTSOURCEV3_H
 
+#include <JANA/Components/JAbstractEventSource.h>
 
-class JEventSourceV3 {
+class JEventSourceV3 : public JAbstractEventSource {
 
+public:
+
+    // ----------------------------------
+    // User is supposed to override these
+    // ----------------------------------
+
+    enum class Result { Success, FailureTryAgain, FailureFinished };
+
+    virtual void Open(const std::string& resource_name, JApplication* app) {}
+
+    virtual Result Next(JEvent& event) = 0;
+
+    virtual void Close() {}
+
+
+
+    // ------------------------
+    // Used internally by JANA
+    // ------------------------
+    // Everything below here should be in a JEventSourceBackend instead
+
+
+    void DoInitialize() final {
+        try {
+            std::call_once(m_init_flag, &JEventSourceV3::Open, this, this->m_application, this->m_resource_name);
+            m_status = SourceStatus::Opened;
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventSourceV3::Open()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+    // TODO: Right now, nobody calls DoClose(). We need DoClose in the case where we are consuming a stream
+    //       from a network socket and somebody calls Stop(). We want the socket to be released cleanly.
+
+    void DoClose() {
+        try {
+            std::call_once(m_finish_flag, &JEventSourceV3::Close, this);
+            m_status = SourceStatus::Finished;
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventSourceV3::Close()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+    ReturnStatus DoNext(std::shared_ptr<JEvent> event) final {
+        Result result;
+        try {
+            switch (m_status) {
+
+                case SourceStatus::Unopened:
+                    DoInitialize(); // Fall-through to Opened afterwards
+
+                case SourceStatus::Opened:
+
+                    switch (Next(*event)) {
+                        case Result::Success:
+                            return ReturnStatus::Success;
+
+                        case Result::FailureTryAgain:
+                            return ReturnStatus::TryAgain;
+
+                        case Result::FailureFinished:
+                            m_status = SourceStatus::Finished;
+                            std::call_once(m_finish_flag, &JEventSourceV3::Close, this);
+                            return ReturnStatus::Finished;
+
+                        default:
+                            throw JException("Invalid ReturnStatus");
+                    }
+
+                case SourceStatus::Finished:
+                    return ReturnStatus::Finished;
+            }
+        }
+        catch (JException& ex) {
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+        catch (...) {
+            auto ex = JException("Unknown exception in JEventSourceV2::GetEvent()");
+            ex.nested_exception = std::current_exception();
+            ex.plugin_name = m_plugin_name;
+            ex.component_name = GetType();
+            throw ex;
+        }
+    }
+
+private:
+    std::once_flag m_init_flag;
+    std::once_flag m_finish_flag;
 };
+
 
 
 #endif //JANA2_JEVENTSOURCEV3_H
