0cb59c02e308059d9b2f487d3283a0bbaf8ffc5c
David Lawrence <davidl@jlab.org>
2020-02-24 13:16:36 -0500

    Sketching out janacontrol plugin for communicating with JANA process via ZMQ

---
+++ src/plugins/janacontrol/tests/IntegrationTests.cc
@@ -0,0 +1,29 @@
+
+#include "catch.hpp"
+
+#include <JANA/JApplication.h>
+#include <JANA/JFactoryGenerator.h>
+
+
+// This is where you can assemble various components and verify that when put together, they
+// do what you'd expect. This means you can skip the laborious mixing and matching of plugins and configurations,
+// and have everything run automatically inside one executable.
+
+TEST_CASE("IntegrationTests") {
+
+    auto app = new JApplication;
+    
+    // Create and register components
+    // app->Add(new janacontrolProcessor);
+
+    // Set test parameters
+    app->SetParameterValue("nevents", 10);
+
+    // Run everything, blocking until finished
+    app->Run();
+
+    // Verify the results you'd expect
+    REQUIRE(app->GetNEventsProcessed() == 10);
+
+}
+
