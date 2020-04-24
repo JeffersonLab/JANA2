847071c3b7db291f0798c79459077fd3a27185c6
Nathan Brei <nbrei@jlab.org>
2020-04-24 07:29:28 -0400

    Add stub for DstExample

---
+++ src/examples/DstExample/DstExample.cc
@@ -0,0 +1,26 @@
+
+
+#include <JANA/JApplication.h>
+#include <JANA/JFactoryGenerator.h>
+
+#include "DstExampleProcessor.h"
+
+extern "C" {
+void InitPlugin(JApplication* app) {
+
+    // This code is executed when the plugin is attached.
+    // It should always call InitJANAPlugin(app) first, and then do one or more of:
+    //   - Read configuration parameters
+    //   - Register JFactoryGenerators
+    //   - Register JEventProcessors
+    //   - Register JEventSourceGenerators (or JEventSources directly)
+    //   - Register JServices
+
+    InitJANAPlugin(app);
+
+    LOG << "Loading DstExample" << LOG_END;
+    app->Add(new DstExampleProcessor);
+    // Add any additional components as needed
+}
+}
+
