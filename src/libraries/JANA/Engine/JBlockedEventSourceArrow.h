789c0b2d0ec6eee5a382003f4297f1fd2c5b15b6
Nathan Brei <nbrei@jlab.org>
2020-09-03 17:27:00 -0400

    WIP: BlockedEventSource

--- src/libraries/JANA/Engine/JBlockedEventSourceArrow.h
+++ src/libraries/JANA/Engine/JBlockedEventSourceArrow.h
@@ -6,28 +6,71 @@
 #define JANA2_JBLOCKEDEVENTSOURCEARROW_H
 
 #include <JANA/Engine/JArrow.h>
+#include <JANA/Engine/JMailbox.h>
 #include <JANA/JBlockedEventSource.h>
 
+template <typename T>
 class JBlockSourceArrow : public JArrow {
-	JBlockedEventSource* m_source;  // non-owning
+	JBlockedEventSource<T>* m_source;  // non-owning
+	JMailbox<T>* m_block_queue; // non-owning
 	JLogger m_logger;
 
 public:
-	JEventSourceArrow(std::string name, JEventSource* source, EventQueue* output_queue, std::shared_ptr<JEventPool> pool);
-	void initialize() final;
-	void execute(JArrowMetrics& result, size_t location_id) final;
+	JBlockSourceArrow(std::string name, JBlockedEventSource<T>* source, JMailbox<T>* block_queue)
+		: JArrow(name, false, NodeType::Source, 1)
+		, m_source(source)
+		, m_block_queue(block_queue)
+	{}
 
+	void initialize() final {
+		LOG_DEBUG(m_logger) << "JBlockDisentanglerArrow '" << get_name() << "': " << "Initializing" << LOG_END;
+		assert(_status == Status::Unopened);
+		//_source->Initialize();
+		_status = Status::Running;
+	}
+	void execute(JArrowMetrics& result, size_t location_id) final {
+
+	}
 };
 
+
+
+template <typename T>
 class JBlockDisentanglerArrow : public JArrow {
-	JBlockedEventSource* m_source;  // non-owning
+	JBlockedEventSource<T>* m_source;  // non-owning
+	JMailbox<T>* m_block_queue; // owning
+	JMailbox<std::shared_ptr<JEvent>>* m_event_queue; // non-owning
 	std::shared_ptr<JEventPool> m_pool;
 	JLogger m_logger;
 
 public:
-	JEventSourceArrow(std::string name, JEventSource* source, EventQueue* output_queue, std::shared_ptr<JEventPool> pool);
-	void initialize() final;
-	void execute(JArrowMetrics& result, size_t location_id) final;
+	JBlockDisentanglerArrow(std::string name,
+							JBlockedEventSource<T>* source,
+							JMailbox<T>* block_queue,
+							JMailbox<std::shared_ptr<JEvent>>* event_queue,
+							std::shared_ptr<JEventPool> pool
+							)
+							: JArrow(std::move(name), true, NodeType::Stage, 1)
+							, m_source(source)
+							, m_block_queue(block_queue)
+							, m_event_queue(event_queue)
+							, m_pool(std::move(pool))
+							{}
+
+	~JBlockDisentanglerArrow() {
+		delete m_block_queue;
+	}
+
+	void initialize() final {
+		LOG_DEBUG(m_logger) << "JBlockDisentanglerArrow '" << get_name() << "': " << "Initializing" << LOG_END;
+		assert(_status == Status::Unopened);
+		//_source->InitializeDisentangler();
+		_status = Status::Running;
+	};
+
+	void execute(JArrowMetrics& result, size_t location_id) final {
+
+	}
 
 };
 
