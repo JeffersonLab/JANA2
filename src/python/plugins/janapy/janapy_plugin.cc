e5df0fbcadc36e121f95f9a48613f6ea33c1cf31
David Lawrence <davidl@jlab.org>
2020-11-14 10:33:00 -0500

    Add script name to sys.argv. Catch exceptions when evaluating file and print more useful error message.

--- src/python/plugins/janapy/janapy_plugin.cc
+++ src/python/plugins/janapy/janapy_plugin.cc
@@ -1,41 +1,7 @@
 //
-//    File: janapy/janapy.cc
-// Created: Fri Dec 21 23:05:11 EST 2018
-// Creator: davidl (on Darwin amelia.jlab.org 17.7.0 i386)
-//
-// ------ Last repository commit info -----
-// [ Date: Tue Feb 26 18:13:39 2019 -0500 ]
-// [ Author: Nathan Brei <nbrei@halld3.jlab.org> ]
-// [ Source: src/plugins/janapy/janapy.cc ]
-// [ Revision: c15aad0b0dec2e6f8f29d1f727b2daec6c7cf376 ]
-//
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Jefferson Science Associates LLC Copyright Notice:
-// Copyright 251 2014 Jefferson Science Associates LLC All Rights Reserved. Redistribution
-// and use in source and binary forms, with or without modification, are permitted as a
-// licensed user provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright notice, this
-//    list of conditions and the following disclaimer in the documentation and/or other
-//    materials provided with the distribution.
-// 3. The name of the author may not be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-// This material resulted from work developed under a United States Government Contract.
-// The Government retains a paid-up, nonexclusive, irrevocable worldwide license in such
-// copyrighted data to reproduce, distribute copies to the public, prepare derivative works,
-// perform publicly and display publicly and to permit others to do so.
-// THIS SOFTWARE IS PROVIDED BY JEFFERSON SCIENCE ASSOCIATES LLC "AS IS" AND ANY EXPRESS
-// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
-// JEFFERSON SCIENCE ASSOCIATES, LLC OR THE U.S. GOVERNMENT BE LIABLE TO LICENSEE OR ANY
-// THIRD PARTES FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-// OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright 2020, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+
 
 #include <thread>
 #include <cstdio>
@@ -49,7 +15,6 @@ using namespace std;
 
 #include <janapy.h>
 #include <pybind11/embed.h>
-
 
 
 void JANA_EmbeddedPythonModuleInit(JApplication *sApp);
@@ -132,9 +97,33 @@ void JANA_EmbeddedPythonModuleInit(JApplication *sApp)
         if( JANA_PYTHON_FILE ) fname = JANA_PYTHON_FILE;
     }
 
+    // Fill in the script name in sys.argv. This is needed for tkinter which
+    // is hardcoded to look at sys.argv[0].
+    std::vector<wchar_t*> argv;
+    auto wfname = std::wstring(fname.begin(), fname.end());
+    argv.push_back( (wchar_t*)wfname.c_str() );
+    PySys_SetArgv(argv.size(), argv.data());
+\
     // Execute python script.
     // n.b. The script may choose to run for the lifetime of the program!
-    py::eval_file(fname);
+    try {
+        py::eval_file(fname);
+    }catch(std::runtime_error &e){
+        std::cerr << std::endl;
+        std::cerr << string(60, '-') << std::endl;
+        std::cerr << "ERROR processing python file: \"" << fname << "\"" << std::endl;
+        std::cerr << e.what() << std::endl;
+        std::cerr << std::endl;
+        if( string(e.what()).find("could not be opened!") != string::npos) {
+            std::cerr << "Please make sure one of the following is true:" << std::endl;
+            std::cerr << "  - The file \"" << fname << "\" exists" << std::endl;
+            std::cerr << "  - The JANA_PYTHON_FILE config. parameter points to a file that exists" << std::endl;
+            std::cerr << "  - The janapy plugin is not in the configuration for the job" << std::endl;
+        }
+        std::cerr << string(60, '-') << std::endl;
+        std::cerr << std::endl;
+        pyjapp->Quit();
+    }
 
     PY_INITIALIZED = true;
 }
