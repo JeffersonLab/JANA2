06ca074c95fef70c29e1def7979890f29fab291d
David Lawrence <davidl@jlab.org>
2020-11-14 10:30:39 -0500

    Fix SetParameterValue to accept non-string types

--- src/python/common/janapy.h
+++ src/python/common/janapy.h
@@ -1,41 +1,7 @@
 //
-//    File: janapy.h
-// Created: Tue Jun 16 14:28:11 EST 2019
-// Creator: davidl (on Darwin amelia.jlab.org 17.7.0 i386)
-//
-// ------ Last repository commit info -----
-// [ Date: Tue Feb 26 18:13:39 2019 -0500 ]
-// [ Author: Nathan Brei <nbrei@halld3.jlab.org> ]
-// [ Source: src/plugins/janapy/janapy.cc ]
-// [ Revision: c15aad0b0dec2e6f8f29d1f727b2daec6c7cf376 ]
-//
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Jefferson Science Associates LLC Copyright Notice:
-// Copyright 251 2014 Jefferson Science Associates LLC All Rights Reserved. Redistribution
-// and use in source and binary forms, with or without modification, are permitted as a
-// licensed user provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright notice, this
-//    list of conditions and the following disclaimer in the documentation and/or other
-//    materials provided with the distribution.
-// 3. The name of the author may not be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-// This material resulted from work developed under a United States Government Contract.
-// The Government retains a paid-up, nonexclusive, irrevocable worldwide license in such
-// copyrighted data to reproduce, distribute copies to the public, prepare derivative works,
-// perform publicly and display publicly and to permit others to do so.
-// THIS SOFTWARE IS PROVIDED BY JEFFERSON SCIENCE ASSOCIATES LLC "AS IS" AND ANY EXPRESS
-// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
-// JEFFERSON SCIENCE ASSOCIATES, LLC OR THE U.S. GOVERNMENT BE LIABLE TO LICENSEE OR ANY
-// THIRD PARTES FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-// OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright 2020, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+
 
 #include <thread>
 #include <cstdio>
@@ -53,13 +19,14 @@ namespace py = pybind11;
 #include "JEventProcessorPY.h"
 
 static bool PY_INITIALIZED = false;
+static bool PY_MODULE_INSTANTIATED_JAPPLICATION = false;
 static JApplication *pyjapp = nullptr;
 
 // Trivial wrappers for JApplication (and friends)
 // n.b. The default values here will NEVER be used. They must be passed in explicitly to
 // pybind11. They are only here for convenience!
 inline void     janapy_Start(void) { PY_INITIALIZED = true; }
-inline void     janapy_Run(void) { pyjapp->Run(); }
+inline void     janapy_Run(void) { if(PY_MODULE_INSTANTIATED_JAPPLICATION) {pyjapp->Run();}else{janapy_Start();} }
 inline void     janapy_Quit(bool skip_join=false) { pyjapp->Quit(skip_join); }
 inline void     janapy_Stop(bool wait_until_idle=false) { pyjapp->Stop(wait_until_idle); }
 inline void     janapy_Resume(void) { pyjapp->Resume(); }
@@ -78,12 +45,31 @@ inline float    janapy_GetInstantaneousRate(void) { return pyjapp->GetInstantane
 inline uint32_t janapy_GetNThreads(void) { return pyjapp->GetNThreads(); }
 inline uint32_t janapy_SetNThreads(int Nthreads) { auto prev = pyjapp->GetNThreads(); pyjapp->Scale(Nthreads); return prev; }
 inline size_t   janapy_GetNcores(void) { return JCpuInfo::GetNumCpus(); }
-inline string   janapy_GetParameterValue(string key) { return pyjapp->GetJParameterManager()->Exists(key) ? pyjapp->GetParameterValue<string>(key):"Not Defined"; }
-inline void     janapy_SetParameterValue(string key, string val) { pyjapp->SetParameterValue<string>( key, val ); }
-
 inline void     janapy_PrintStatus(void) { pyjapp->PrintStatus(); }
 inline void     janapy_PrintParameters(bool all=false) { pyjapp->GetJParameterManager()->PrintParameters(all); }
+inline string   janapy_GetParameterValue(string key) { return pyjapp->GetJParameterManager()->Exists(key) ? pyjapp->GetParameterValue<string>(key):"Not Defined"; }
 
+// Not sure of the clean way to have python cast val object to string before passing to us ...
+inline void     janapy_SetParameterValue(string key, py::object valobj)
+{
+//    const char* val = PyUnicode_AsUTF8( PyObject_Str(valobj) );
+    stringstream ss;
+    try {
+        auto val = valobj.cast<string>();
+        ss << val;
+    }catch(...){
+        try {
+            auto val = valobj.cast<double>();
+            ss << val;
+        }catch(...) {
+            try {
+                auto val = valobj.cast<long>();
+                ss << val;
+            }catch(...){}
+        }
+    }
+    pyjapp->SetParameterValue<string>( key, ss.str() );
+}
 
 inline void janapy_AddProcessor(py::object &pyproc )
 {
@@ -133,12 +119,12 @@ m.def("GetInstantaneousRate",        &janapy_GetInstantaneousRate,        "Retur
 m.def("GetNThreads",                 &janapy_GetNThreads,                 "Return current number of JThread objects."); \
 m.def("SetNThreads",                 &janapy_SetNThreads,                 "Set number of JThread objects. (returns previous number)"); \
 m.def("GetNcores",                   &janapy_GetNcores,                   "Return number of cores reported by system (full + logical)."); \
+m.def("PrintStatus",                 &janapy_PrintStatus,                 "Print the status of the current job to the screen"); \
+m.def("PrintParameters",             &janapy_PrintParameters,             "Print config. parameters (give argument True to print all parameters)"); \
 m.def("GetParameterValue",           &janapy_GetParameterValue,           "Return value of given configuration parameter."); \
 m.def("SetParameterValue",           &janapy_SetParameterValue,           "Set configuration parameter."); \
 m.def("AddProcessor",                &janapy_AddProcessor,                "Add an event processor"); \
 \
-m.def("PrintStatus",                 &janapy_PrintStatus,                 "Print the status of the current job to the screen"); \
-m.def("PrintParameters",             &janapy_PrintParameters,             "Print config. parameters (give argument True to print all parameters)"); \
 
 //================================================================================
 
