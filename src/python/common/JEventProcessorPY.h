7efeb60c19b7ee27b1cc47b7f6f614ae0d40bda0
David Lawrence <davidl@jlab.org>
2021-03-11 16:26:46 -0500

    Create JEventProcessPYTrampoline class to address issue with both JANA and pybind11 wanting to own the JEventProcessor objects.

--- src/python/common/JEventProcessorPY.h
+++ src/python/common/JEventProcessorPY.h
@@ -2,6 +2,21 @@
 #ifndef _JEVEVENTPROCESSOR_PY_H_
 #define _JEVEVENTPROCESSOR_PY_H_
 
+// This defines the JEventProcessorPY classes used to implement
+// a JEventProcessor class in Python. The design uses a pair of
+// classes with a HasA relationship.
+//
+// The JEventProcessorPY class is a C++ class that inherits from
+// JEventProcessor.
+//
+// The JEventProcessorPYpyobject class is a python class that
+// inherits from pyobject in pybind11.
+//
+// Two classes are needed because the design of JANA requires
+// ownership of the JEventProcessor object be given to JApplication.
+// At the same time, pybind11 insists on ownership of all pyobjects.
+
+
 #include <mutex>
 #include <iostream>
 using std::cout;
@@ -14,7 +29,9 @@ namespace py = pybind11;
 #include <JANA/JEventProcessor.h>
 #include <JANA/JEvent.h>
 
-class JEventProcessorPY : public JEventProcessor{
+//#pragma GCC visibility push(hidden)
+
+class JEventProcessorPY {
 
     public:
 
@@ -79,4 +96,18 @@ class JEventProcessorPY : public JEventProcessor{
 
 };
 
+class JEventProcessorPYTrampoline: public JEventProcessor {
+
+public:
+    JEventProcessorPYTrampoline(JEventProcessorPY *jevent_proc):jevent_proc_py(jevent_proc){
+        SetTypeName("JEventProcessorPY");
+    }
+
+    void Init(void){ jevent_proc_py->Init(); }
+    void Process(const std::shared_ptr<const JEvent>& aEvent){ jevent_proc_py->Process(aEvent); }
+    void Finish(void){ jevent_proc_py->Finish(); }
+
+private:
+    JEventProcessorPY *jevent_proc_py = nullptr;
+};
 #endif  // _JEVEVENTPROCESSOR_PY_H_
