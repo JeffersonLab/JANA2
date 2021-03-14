7832b937e0728424628dbd22b8ccd99860ab4092
David Lawrence <davidl@jlab.org>
2021-03-14 09:52:36 -0400

    This implements the Prefetch feature of the Python API, but also starts implementing the Get feature. Saving work before moving ROOT object stringification into JService.

--- src/python/common/JEventProcessorPY.h
+++ src/python/common/JEventProcessorPY.h
@@ -29,9 +29,18 @@ using std::endl;
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
+#ifdef HAVE_ROOT
+#include <TObject.h>
+#include <TClass.h>
+#include <TDataMember.h>
+#include <TMethodCall.h>
+#include <Tlist.h>
+#endif // HAVE_ROOT
 
 #include <JANA/JEventProcessor.h>
 #include <JANA/JEvent.h>
+#include <janacontrol/src/janaJSON.h>
+#include <janacontrol/src/JControlEventProcessor.h>
 
 //#pragma GCC visibility push(hidden)
 
@@ -57,6 +66,8 @@ class JEventProcessorPY {
         cout << "JEventProcessorPY destructor called : " << this  << endl;
     }
 
+    //----------------------------------------
+    // Init
     void Init(void){
 
         cout << "JEventProcessorPY::Init called " << endl;
@@ -67,21 +78,54 @@ class JEventProcessorPY {
 
     }
 
+    //----------------------------------------
+    // Process
     void Process(const std::shared_ptr<const JEvent>& aEvent){
 
         cout << "JEventProcessorPY::Process called " << endl;
 
-        // According to the Python documentation we should be wrapping the call to pmProcess() below
-        // in the following that activate the GIL lock. In practice, this seemed to allow each thread
-        // to call pymProcess(), once, but then the program stalled. Hence, we use our own mutex.
-        // PyGILState_STATE gstate = PyGILState_Ensure();
-        // PyGILState_Release(gstate);
+        // Prefetch any factory in the prefetch list.
+        // This does not actually fetch them, but does activate the factories
+        // to ensure the objects are already created before locking the mutex.
+        // We try fetching as both JObject and TObject for the rare case when
+        // the objects may be TObjects but not JObjects. We cannot currently
+        // handle the case where the factory object is neither a JObject nor
+        // a TObject.
+        for( auto p : prefetch_factories ){
+            auto fac = aEvent->GetFactory( p.first, p.second);
+            if( fac == nullptr ){
+                jerr << "Unable to find factory specified for prefetching: factory=" << p.first << " tag=" << p.second << std::endl;
+            }else {
+                auto v = fac->GetAs<JObject>();
+#ifdef HAVE_ROOT
+                if( v.empty() )fac->GetAs<TObject>();
+#endif // HAVE_ROOT
+            }
+        }
+
         if( has_pymProcess ) {
+
+            // According to the Python documentation we should be wrapping the call to pmProcess() below
+            // in the following that activates the GIL lock. In practice, this seemed to allow each thread
+            // to call pymProcess(), once, but then the program stalled. Hence, we use our own mutex.
+            // PyGILState_STATE gstate = PyGILState_Ensure();
+            // PyGILState_Release(gstate);
             lock_guard<mutex> lck(pymutex);
+
+            // This magic line creates a shared_ptr on the stack with a custom deleter.
+            // The custom deleter is used to reset the _aEvent data member back to
+            // nullptr so that it does not hold on the the JEvent after we return
+            // from this method. We use this trick to ensure it happens even if an
+            // exception is thrown from pymProcess().
+            std::shared_ptr<int> _avent_free(nullptr, [=](int *ptr){_aEvent = nullptr;});
+            _aEvent = aEvent; // remember this JEvent so it can be used in calls to Get()
+
             pymProcess();
         }
     }
 
+    //----------------------------------------
+    // Finish
     void Finish(void){
 
         cout << "JEventProcessorPY::Finish called " << endl;
@@ -93,6 +137,151 @@ class JEventProcessorPY {
 
     }
 
+    //----------------------------------------
+    // Prefetch
+    void Prefetch(py::object &fac_name, py::object tag = py::none()){
+
+        /// This is called from python to register factories to be activated
+        /// before the Process method of the python JEventProcessor class
+        /// is called. Since the python code must be executed in serial, this
+        /// allows object generation by factories to be done in parallel before
+        /// that to try and minimize the time in serial operations.
+        ///
+        /// When calling from python, the first argument may be any of a
+        /// string, list, or dictionary. The second argument is for the optional
+        /// factory tag which is only considered if the first argument is a
+        /// string.
+        ///
+        /// If the first argument is a string, it is taken as the data type to
+        /// prefetch.
+        ///
+        /// If the first argument is a list, it is taken to be the names of multiple
+        /// data types to prefetch. For this case, the default (i.e. empty) factory
+        /// tag is used for all of them.
+        ///
+        /// If the first argument is a dictionary, then the keys are taken as
+        /// the data types and corresponding values taken as the factory tags.
+
+        if( py::isinstance<py::dict>(fac_name) ){
+            // Python dictionary was passed
+            for(auto p : fac_name.cast<py::dict>()){
+                auto &fac_obj = p.first;
+                auto &tag_obj = p.second;
+                std::string fac_name_str = py::str(fac_obj);
+                std::string tag_str = tag_obj.is(py::none()) ? "":py::str(tag_obj);
+                prefetch_factories[fac_name_str] = tag_str;
+            }
+        }else if( py::isinstance<py::list>(fac_name) ){
+            // Python list was passed
+            for(auto &fac_obj : fac_name.cast<py::list>()){
+                std::string fac_name_str = py::str(fac_obj);
+                prefetch_factories[fac_name_str] = "";
+            }
+        }else if( py::isinstance<py::str>(fac_name) ){
+            // Python string was passed
+            std::string fac_name_str = py::str(fac_name);
+            std::string tag_str = tag.is(py::none()) ? "":py::str(tag);
+            prefetch_factories[fac_name_str] = tag_str;
+        }else{
+            jerr << "Unknown type passed to Prefetch: " << std::string(py::str(fac_name)) << std::endl;
+        }
+    }
+
+    //----------------------------------------
+    // Get
+    py::object Get(py::object &fac_name, py::object tag = py::none()) {
+        std::string fac_name_str = py::str(fac_name);
+        std::string tag_str = tag.is(py::none()) ? "":py::str(tag);
+
+        std::map<std::string, JObjectSummary> objects;
+        GetObjectSummaries(fac_name_str, tag_str, objects);
+
+        py::list list;
+        for(auto p : objects){
+
+            const std::string &hexaddr = p.first;
+            const JObjectSummary &summary = p.second;
+//            std::string obj_json = JJSON_Create(summary);
+            std::string obj_json = ObjectToJSON( hexaddr, summary );
+
+            try {
+                auto json_loads = pymodule_json->attr("loads" );
+                auto dict = json_loads( py::str(obj_json) );
+                list.append( dict );
+            }catch(...){
+                jerr << "Python json loads function not available!" << std::endl;
+            }
+        }
+
+        return list;
+    }
+
+    //----------------------------------------
+    // GetObjectSummaries
+    //
+    // TODO: Take this and the (same) code in JControlEventPRocessor and move them to a common place
+    void GetObjectSummaries(const std::string &factory_name, const std::string &factory_tag, std::map<std::string, JObjectSummary> &objects){
+        // bombproof against getting called with no active JEvent
+        if(_aEvent.get() == nullptr ) return;
+        auto fac = _aEvent->GetFactory(factory_name, factory_tag);
+        if( fac == nullptr ){
+            jerr << "Unable to find factory specified for prefetching: factory=" << factory_name << " tag=" << factory_tag << std::endl;
+            return;
+        }
+        for( auto jobj : fac->GetAs<JObject>()){
+            JObjectSummary summary;
+            jobj->Summarize(summary);
+            std::stringstream ss;
+            ss << "0x" << std::hex << (uint64_t)jobj << std::dec;
+            objects[ss.str()] = summary; // key is address of object converted to string
+        }
+#ifdef HAVE_ROOT
+        // For objects inheriting from TObject, we try and convert members automatically
+        // into JObjectSummary form. This relies on dictionaries being compiled in.
+        // (see ROOT_GENERATE_DICTIONARY for cmake files).
+        for( auto tobj : fac->GetAs<TObject>()){
+            JObjectSummary summary;
+            auto tclass = TClass::GetClass(tobj->ClassName());
+            if(tclass){
+                auto *members = tclass->GetListOfAllPublicDataMembers();
+                for( auto item : *members){
+                    TDataMember *memitem = dynamic_cast<TDataMember*>(item);
+                    if( memitem == nullptr ) continue;
+                    if( memitem->Property() & kIsStatic ) continue; // exclude TObject enums
+                    JObjectMember jObjectMember;
+                    jObjectMember.name = memitem->GetName();
+                    jObjectMember.type = memitem->GetTypeName();
+                    jObjectMember.value = GetRootObjectMemberAsString(tobj, memitem, jObjectMember.type);
+                    summary.add(jObjectMember);
+                }
+            }else {
+                LOG << "Unable to get TClass for: " << tobj->ClassName() << LOG_END;
+            }
+            std::stringstream ss;
+            ss << "0x" << std::hex << (uint64_t)tobj << std::dec;
+            objects[ss.str()] = summary; // key is address of object converted to string
+        }
+#endif
+     }
+
+    //----------------------------------------
+    // ObjectToJSON
+    std::string ObjectToJSON( const std::string &hexaddr, const JObjectSummary &summary ){
+
+        stringstream ss;
+        ss << "{\n";
+        ss << "\"hexaddr\":\"" << hexaddr << "\"\n";
+        for( auto m : summary.get_fields() ){
+            ss << ",\"" << m.name << "\":\"" << m.value << "\"\n";
+        }
+        ss << "}";
+
+        return std::move(ss.str());
+    }
+
+    // Data members
+    py::module_ *pymodule = nullptr;       // This gets set in janapy_AddProcessor
+    py::module_ *pymodule_json = nullptr;  // This gets set in janapy_AddProcessor
     std::string class_name = "JEventProcssorPY";
     py::object &pyobj; // _self_
     py::object pymInit;
@@ -104,6 +293,8 @@ class JEventProcessorPY {
 
     mutex pymutex;
 
+    std::shared_ptr<const JEvent> _aEvent;
+    std::map<std::string, std::string> prefetch_factories;
 };
 
 class JEventProcessorPYTrampoline: public JEventProcessor {
