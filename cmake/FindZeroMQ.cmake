f20a559a145cb7bab2c424f0a62c1ade19b8b8d1
David Lawrence <davidl@jlab.org>
2022-06-24 08:01:01 -0400

    Set the ZeroMQ_DIR variable which had been made a requirement, but somehow was not being set anywhere

--- cmake/FindZeroMQ.cmake
+++ cmake/FindZeroMQ.cmake
@@ -4,16 +4,31 @@
 # ZeroMQ_INCLUDE_DIRS - The ZeroMQ include directories
 # ZeroMQ_LIBRARIES - The libraries needed to use ZeroMQ
 # ZeroMQ_DEFINITIONS - Compiler switches required for using ZeroMQ
+
 find_path (ZeroMQ_INCLUDE_DIR zmq.h)
 find_library (ZeroMQ_LIBRARY NAMES zmq)
 set (ZeroMQ_LIBRARIES ${ZeroMQ_LIBRARY})
 set (ZeroMQ_INCLUDE_DIRS ${ZeroMQ_INCLUDE_DIR})
-include (FindPackageHandleStandardArgs)
+
+# Use parent directory name of the include directory to
+# indicate where we are getting this from. This is
+# informational only for when the top-level file
+# prints "USE_ZEROMQ  On  -->"
+# n.b. it is possible the library and include directories
+# don't point to the same version!
+get_filename_component(ZeroMQ_DIR ${ZeroMQ_INCLUDE_DIR} DIRECTORY)
+
+# For debugging
+#message(STATUS "ZeroMQ_INCLUDE_DIR=${ZeroMQ_INCLUDE_DIR}")
+#message(STATUS "ZeroMQ_LIBRARY=${ZeroMQ_LIBRARY}")
+#message(STATUS "ZeroMQ_DIR=${ZeroMQ_DIR}")
+
 # handle the QUIETLY and REQUIRED arguments and set ZeroMQ_FOUND to TRUE
 # if all listed variables are TRUE
 
+include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ZeroMQ
         FOUND_VAR ZeroMQ_FOUND
         VERSION_VAR ZeroMQ_VERSION
-        REQUIRED_VARS ZeroMQ_DIR ZeroMQ_INCLUDE_DIR ZeroMQ_LIBRARY
+        REQUIRED_VARS ZeroMQ_DIR ZeroMQ_INCLUDE_DIRS ZeroMQ_LIBRARIES
         )
