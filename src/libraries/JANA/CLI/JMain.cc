ba408bd3806bccd6b9165b2ae9d20a9e003d0e7b
Nathan Brei <nbrei@jlab.org>
2022-10-15 17:42:59 -0400

    Show JANA version on startup

--- src/libraries/JANA/CLI/JMain.cc
+++ src/libraries/JANA/CLI/JMain.cc
@@ -83,19 +83,12 @@ JApplication* CreateJApplication(UserOptions& options) {
 int Execute(JApplication* app, UserOptions &options) {
 
     std::cout << std::endl;
-    std::cout << "     ____      _     ___      ___       _               \n"
-		 "     `MM'     dM.    `MM\\     `M'      dM.              \n"
-		 "      MM     ,MMb     MMM\\     M      ,MMb              \n"
-		 "      MM     d'YM.    M\\MM\\    M      d'YM.      ____   \n"
-		 "      MM    ,P `Mb    M \\MM\\   M     ,P `Mb     6MMMMb  \n"
-		 "      MM    d'  YM.   M  \\MM\\  M     d'  YM.   MM'  `Mb \n"
-		 "      MM   ,P   `Mb   M   \\MM\\ M    ,P   `Mb        ,MM \n"
-		 "      MM   d'    YM.  M    \\MM\\M    d'    YM.      ,MM' \n"
-		 "(8)   MM  ,MMMMMMMMb  M     \\MMM   ,MMMMMMMMb    ,M'    \n"
-		 "((   ,M9  d'      YM. M      \\MM   d'      YM. ,M'      \n"
-		 " YMMMM9 _dM_     _dMM_M_      \\M _dM_     _dMM_MMMMMMMM " << std::endl << std::endl;
-
-    // std::cout << "JANA " << JVersion::GetVersion() << " [" << JVersion::GetRevision() << "]" << std::endl;
+    std::cout << "       |    \\      \\  |     \\    ___ \\   " << std::endl;
+    std::cout << "       |   _ \\      \\ |    _ \\      ) |" << std::endl;
+    std::cout << "   \\   |  ___ \\   |\\  |   ___ \\    __/" << std::endl;
+    std::cout << "  \\___/ _/    _\\ _| \\_| _/    _\\ _____|" << std::endl;
+    std::cout << std::endl;
+    std::cout << "JANA2 " << JVersion::GetVersion() << " [" << JVersion::GetRevision() << "]" << std::endl;
     JSignalHandler::register_handlers(app);
 
     if (options.flags[ShowConfigs]) {
