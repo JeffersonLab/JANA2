cea7280a3bce147c5efdd010bcc8513900b858cf
Nathan Brei <nbrei@jlab.org>
2023-01-31 15:16:54 -0500

    Add stub PodioExample

---
+++ src/examples/PodioExample/PodioExample.cc
@@ -0,0 +1,9 @@
+
+// Copyright 2023, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+#include <iostream>
+#include <podio/CollectionBase.h>
+int main() {
+    std::cout << "Hello world" << std::endl;
+    podio::CollectionBase* base = nullptr;
+}
