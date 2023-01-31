18219ac6112df91dede4bd4b2d4b09a3efdda594
Nathan Brei <nbrei@jlab.org>
2023-01-31 16:10:05 -0500

    PodioExample compiles against generated datamodel

--- src/examples/PodioExample/PodioExample.cc
+++ src/examples/PodioExample/PodioExample.cc
@@ -3,7 +3,18 @@
 // Subject to the terms in the LICENSE file found in the top-level directory.
 #include <iostream>
 #include <podio/CollectionBase.h>
+#include "datamodel/MutableExampleHit.h"
+#include "datamodel/ExampleHitCollection.h"
+
 int main() {
     std::cout << "Hello world" << std::endl;
-    podio::CollectionBase* base = nullptr;
+    auto coll = new ExampleHitCollection;
+    MutableExampleHit hit;
+    hit.cellID(22);
+    hit.energy(100.0);
+    hit.x(0);
+    hit.y(0);
+    hit.z(0);
+    coll->push_back(hit);
+
 }
