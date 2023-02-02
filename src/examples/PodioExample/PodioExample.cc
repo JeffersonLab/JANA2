e2e2e341f0973c22db4c953c16d568632d502af3
Nathan Brei <nbrei@jlab.org>
2023-02-02 11:58:46 -0500

    Use PodioTypeMap in JANA-like context

--- src/examples/PodioExample/PodioExample.cc
+++ src/examples/PodioExample/PodioExample.cc
@@ -10,6 +10,48 @@
 #include <podio/ROOTFrameReader.h>
 #include "DatamodelGlue.h"
 
+struct FakeJANA {
+    podio::Frame m_frame;
+    std::vector<const ExampleHit*> m_cache;
+
+    void Process() {
+        auto coll = new ExampleHitCollection;
+        MutableExampleHit hit;
+        hit.cellID(22);
+        hit.energy(100.0);
+        hit.x(0);
+        hit.y(0);
+        hit.z(0);
+        std::cout << hit << std::endl;
+        coll->push_back(hit);
+        SetCollection<ExampleHit>(coll, "hits");
+
+    }
+
+    template <typename T>
+    void SetCollection(typename PodioTypeMap<T>::collection_t* coll, std::string coll_name) {
+        const auto& moved = m_frame.put(std::move(*coll), coll_name);
+        for (const ExampleHit& item : moved) {
+            m_cache.push_back(&item); // Is this stable?
+        }
+    }
+
+    template <typename T>
+    void Set(const std::vector<T*>& hits) {
+        auto coll = new typename PodioTypeMap<T>::collection_t;
+        for (T* hit : hits) {
+            coll->push_back(hit);
+        }
+    }
+
+    template <typename T>
+    const typename PodioTypeMap<T>::collection_t& GetCollection() {
+        return m_frame.get<PodioTypeMap<T>::collection_t>("FakeFactory");
+    }
+
+
+};
+
 int main() {
     std::cout << "Hello world" << std::endl;
     auto coll = new ExampleHitCollection;
