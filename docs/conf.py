40ca04282280353f8c9f27fecb82415aadb85472
harperma1 <137920636+harperma1@users.noreply.github.com>
2023-06-28 14:45:51 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,1 @@
-python:
-  install:
-    - requirements: docs/requirements.txt
-    - method: pip
-      path: .
-      extra_requirements:
-        - docs
-    - method: pip
-      path: another/package
-  system_packages: true
+
