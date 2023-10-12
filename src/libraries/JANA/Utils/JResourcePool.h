71ae29b1011a83feb842e350eb7936889d85b273
Nathan Brei <nbrei@jlab.org>
2023-10-12 11:11:35 -0400

    Excise JResourcePool::Get_CacheLineSize

--- src/libraries/JANA/Utils/JResourcePool.h
+++ src/libraries/JANA/Utils/JResourcePool.h
@@ -89,18 +89,6 @@ template <typename DType> class JResourcePool
         std::size_t Get_PoolSize(void) const;
         std::size_t Get_NumObjectsAllThreads(void) const{return dObjectCounter;}
 
-        static constexpr unsigned int Get_CacheLineSize(void)
-        {
-            /// Returns the cache line size for the processor of the target platform.
-            /*! The cache line size is useful for creating a buffer to make sure that a variable accessed by multiple threads does not share the cache line it is on.
-                This is useful for variables that may be written-to by one of the threads, because the thread will acquire locked access to the entire cache line.
-                This blocks other threads from operating on the other data stored on the cache line. Note that it is also important to align the shared data as well.
-                See http://www.drdobbs.com/parallel/eliminate-false-sharing/217500206?pgno=4 for more details. */
-
-            //cache line size is 64 for ifarm1402, gcc won't allow larger than 128
-            //the cache line size is in /sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size
-            return JANA2_CACHE_LINE_BYTES ; //units are in bytes
-        }
 
     private:
 
