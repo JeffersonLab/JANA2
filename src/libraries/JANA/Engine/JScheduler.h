b48929ce6f6ca94cb9738702cdf0163df89032a5
Nathan Brei <nbrei@jlab.org>
2023-10-20 15:31:24 -0400

    Reformat indentation in JScheduler.h

--- src/libraries/JANA/Engine/JScheduler.h
+++ src/libraries/JANA/Engine/JScheduler.h
@@ -11,35 +11,35 @@
 #include <JANA/Services/JLoggingService.h>
 
 
-    struct JArrowTopology;
+struct JArrowTopology;
 
-    /// Scheduler assigns Arrows to Workers in a first-come-first-serve manner,
-    /// not unlike OpenMP's `schedule dynamic`.
-    class JScheduler {
+/// Scheduler assigns Arrows to Workers in a first-come-first-serve manner,
+/// not unlike OpenMP's `schedule dynamic`.
+class JScheduler {
 
-    private:
-        std::shared_ptr<JArrowTopology> m_topology;
-        size_t m_next_idx;
-        std::mutex m_mutex;
+private:
+    std::shared_ptr<JArrowTopology> m_topology;
+    size_t m_next_idx;
+    std::mutex m_mutex;
 
-    public:
+public:
 
-        /// Constructor. Note that a Scheduler operates on a vector of Arrow*s.
-        JScheduler(std::shared_ptr<JArrowTopology> topology);
+    /// Constructor. Note that a Scheduler operates on a vector of Arrow*s.
+    JScheduler(std::shared_ptr<JArrowTopology> topology);
 
-        /// Lets a Worker ask the Scheduler for another assignment. If no assignments make sense,
-        /// Scheduler returns nullptr, which tells that Worker to idle until his next checkin.
-        /// If next_assignment() makes any changes to internal Scheduler state or to any of its arrows,
-        /// it must be synchronized.
-        JArrow* next_assignment(uint32_t worker_id, JArrow* assignment, JArrowMetrics::Status result);
+    /// Lets a Worker ask the Scheduler for another assignment. If no assignments make sense,
+    /// Scheduler returns nullptr, which tells that Worker to idle until his next checkin.
+    /// If next_assignment() makes any changes to internal Scheduler state or to any of its arrows,
+    /// it must be synchronized.
+    JArrow* next_assignment(uint32_t worker_id, JArrow* assignment, JArrowMetrics::Status result);
 
-        /// Lets a Worker tell the scheduler that he is shutting down and won't be working on his assignment
-        /// any more. The scheduler is thus free to reassign the arrow to one of the remaining workers.
-        void last_assignment(uint32_t worker_id, JArrow* assignment, JArrowMetrics::Status result);
+    /// Lets a Worker tell the scheduler that he is shutting down and won't be working on his assignment
+    /// any more. The scheduler is thus free to reassign the arrow to one of the remaining workers.
+    void last_assignment(uint32_t worker_id, JArrow* assignment, JArrowMetrics::Status result);
 
-        /// Logger is public so that somebody else can configure it
-        JLogger logger;
-    };
+    /// Logger is public so that somebody else can configure it
+    JLogger logger;
+};
 
 
 #endif // _JSCHEDULER_H_
