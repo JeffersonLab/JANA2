bf1be32e67d6ba6995e4c3740dca611e34d1f185
Nathan Brei <nbrei@jlab.org>
2023-12-06 23:26:08 -0500

    Improve naming inside JMailbox

--- src/libraries/JANA/Engine/JMailbox.h
+++ src/libraries/JANA/Engine/JMailbox.h
@@ -20,47 +20,47 @@
 ///   - the underlying queue may be shared by all threads, NUMA-domain-local, or thread-local
 ///   - the Arrow doesn't have to know anything about locality.
 ///
-/// To handle memory locality at different granularities, we introduce the concept of a domain.
-/// Each thread belongs to exactly one domain. Domains are represented by contiguous unsigned
+/// To handle memory locality at different granularities, we introduce the concept of a location.
+/// Each thread belongs to exactly one location, represented by contiguous unsigned
 /// ints starting at 0. While JArrows are wired to one logical JMailbox, JWorkers interact with
-/// the physical DomainLocalMailbox corresponding to their very own memory domain.
+/// the physical LocalQueue corresponding to their location. Locations prevent events from crossing 
+/// NUMA domains as they get picked up by different JWorker threads.
 ///
 /// \tparam T must be moveable. Usually this is unique_ptr<JEvent>.
 ///
 /// Improvements:
-///   1. Pad DomainLocalMailbox
+///   1. Pad LocalQueue
 ///   2. Enable work stealing
-///   3. Triple mutex trick to give push() priority?
 
 
 class JQueue {
 protected:
-    size_t m_threshold;
+    size_t m_capacity;
     size_t m_locations_count;
     bool m_enable_work_stealing = false;
 
 public:
-    inline size_t get_threshold() { return m_threshold; }
+    inline size_t get_threshold() { return m_capacity; }
     inline size_t get_locations_count() { return m_locations_count; }
     inline bool is_work_stealing_enabled() { return m_enable_work_stealing; }
 
 
     inline JQueue(size_t threshold, size_t locations_count, bool enable_work_stealing)
-        : m_threshold(threshold), m_locations_count(locations_count), m_enable_work_stealing(enable_work_stealing) {}
+        : m_capacity(threshold), m_locations_count(locations_count), m_enable_work_stealing(enable_work_stealing) {}
     virtual ~JQueue() = default;
 };
 
 template <typename T>
 class JMailbox : public JQueue {
 
-    struct LocalMailbox {
+    struct LocalQueue {
         std::mutex mutex;
         std::deque<T> queue;
         size_t reserved_count = 0;
     };
 
     // TODO: Copy these params into DLMB for better locality
-    std::unique_ptr<LocalMailbox[]> m_mailboxes;
+    std::unique_ptr<LocalQueue[]> m_queues;
 
 public:
 
@@ -79,37 +79,37 @@ public:
 
 
     /// threshold: the (soft) maximum number of items in the queue at any time
-    /// domain_count: the number of domains
-    /// enable_work_stealing: allow domains to pop from other domains' queues when theirs is empty
+    /// locations_count: the number of locations. More locations = better NUMA performance, worse load balancing
+    /// enable_work_stealing: allow events to cross locations only when no other work is available. Improves aforementioned load balancing.
     JMailbox(size_t threshold=100, size_t locations_count=1, bool enable_work_stealing=false)
         : JQueue(threshold, locations_count, enable_work_stealing) {
 
-        m_mailboxes = std::unique_ptr<LocalMailbox[]>(new LocalMailbox[locations_count]);
+        m_queues = std::unique_ptr<LocalQueue[]>(new LocalQueue[locations_count]);
     }
 
     virtual ~JMailbox() {
-        //delete [] m_mailboxes;
+        //delete [] m_queues;
     }
 
     // We can do this (for now) because we use a deque underneath, so threshold is 'soft'
-    inline void set_threshold(size_t threshold) { m_threshold = threshold; }
+    inline void set_threshold(size_t threshold) { m_capacity = threshold; }
 
-    /// size() counts the number of items in the queue across all domains
+    /// size() counts the number of items in the queue across all locations
     /// This should be used sparingly because it will mess up a bunch of caches.
     /// Meant to be used by measure_perf()
     size_t size() {
         size_t result = 0;
         for (size_t i = 0; i<m_locations_count; ++i) {
-            std::lock_guard<std::mutex> lock(m_mailboxes[i].mutex);
-            result += m_mailboxes[i].queue.size();
+            std::lock_guard<std::mutex> lock(m_queues[i].mutex);
+            result += m_queues[i].queue.size();
         }
         return result;
     };
 
-    /// size(domain) counts the number of items in the queue for a particular domain
+    /// size(location_id) counts the number of items in the queue for a particular location
     /// Meant to be used by Scheduler::next_assignment() and measure_perf(), eventually
-    size_t size(size_t domain) {
-        return m_mailboxes[domain].queue.size();
+    size_t size(size_t location_id) {
+        return m_queues[location_id].queue.size();
     }
 
     /// reserve(requested_count) keeps our queues bounded in size. The caller should
@@ -119,11 +119,11 @@ public:
     /// reserved on the output queue. Note that because the input queue may return
     /// fewer items than requested, the caller must push their original reserved_count
     /// alongside the items, to avoid a "reservation leak".
-    size_t reserve(size_t requested_count, size_t domain = 0) {
+    size_t reserve(size_t requested_count, size_t location_id = 0) {
 
-        LocalMailbox& mb = m_mailboxes[domain];
+        LocalQueue& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
-        size_t doable_count = m_threshold - mb.queue.size() - mb.reserved_count;
+        size_t doable_count = m_capacity - mb.queue.size() - mb.reserved_count;
         if (doable_count > 0) {
             size_t reservation = std::min(doable_count, requested_count);
             mb.reserved_count += reservation;
@@ -132,32 +132,32 @@ public:
         return 0;
     };
 
-    /// push(items, reserved_count, domain) This function will always
+    /// push(items, reserved_count, location_id) This function will always
     /// succeed, although it may exceed the threshold if the caller didn't reserve
     /// space, and it may take a long time because it will wait on a mutex.
     /// Note that if the caller had called reserve(), they must pass in the reserved_count here.
-    Status push(std::vector<T>& buffer, size_t reserved_count = 0, size_t domain = 0) {
+    Status push(std::vector<T>& buffer, size_t reserved_count = 0, size_t location_id = 0) {
 
-        auto& mb = m_mailboxes[domain];
+        auto& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
         mb.reserved_count -= reserved_count;
         for (const T& t : buffer) {
              mb.queue.push_back(std::move(t));
         }
         buffer.clear();
-        if (mb.queue.size() > m_threshold) {
+        if (mb.queue.size() > m_capacity) {
             return Status::Full;
         }
         return Status::Ready;
     }
 
 
-    /// pop() will pop up to requested_count items for the desired domain.
+    /// pop() will pop up to requested_count items for the desired location_id.
     /// If many threads are contending for the queue, this will fail with Status::Contention,
     /// in which case the caller should probably consult the Scheduler.
     Status pop(std::vector<T>& buffer, size_t requested_count, size_t location_id = 0) {
 
-        auto& mb = m_mailboxes[location_id];
+        auto& mb = m_queues[location_id];
         if (!mb.mutex.try_lock()) {
             return Status::Congested;
         }
@@ -169,7 +169,7 @@ public:
         }
         auto size = mb.queue.size();
         mb.mutex.unlock();
-        if (size >= m_threshold) {
+        if (size >= m_capacity) {
             return Status::Full;
         }
         else if (size != 0) {
@@ -182,7 +182,7 @@ public:
     Status pop(T& item, bool& success, size_t location_id = 0) {
 
         success = false;
-        auto& mb = m_mailboxes[location_id];
+        auto& mb = m_queues[location_id];
         if (!mb.mutex.try_lock()) {
             return Status::Congested;
         }
@@ -208,10 +208,10 @@ public:
 
 
 
-    bool try_push(T* buffer, size_t count, size_t domain = 0) {
-        auto& mb = m_mailboxes[domain];
+    bool try_push(T* buffer, size_t count, size_t location_id = 0) {
+        auto& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
-        if (mb.queue.size() + count > m_threshold) return false;
+        if (mb.queue.size() + count > m_capacity) return false;
         for (size_t i=0; i<count; ++i) {
              mb.queue.push_back(buffer[i]);
              buffer[i] = nullptr;
@@ -219,12 +219,12 @@ public:
         return true;
     }
 
-    void push_and_unreserve(T* buffer, size_t count, size_t reserved_count = 0, size_t domain = 0) {
+    void push_and_unreserve(T* buffer, size_t count, size_t reserved_count = 0, size_t location_id = 0) {
 
-        auto& mb = m_mailboxes[domain];
+        auto& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
         mb.reserved_count -= reserved_count;
-        assert(mb.queue.size() + count <= m_threshold);
+        assert(mb.queue.size() + count <= m_capacity);
         for (size_t i=0; i<count; ++i) {
              mb.queue.push_back(buffer[i]);
              buffer[i] = nullptr;
@@ -233,7 +233,7 @@ public:
 
     size_t pop(T* buffer, size_t min_requested_count, size_t max_requested_count, size_t location_id = 0) {
 
-        auto& mb = m_mailboxes[location_id];
+        auto& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
 
         if (mb.queue.size() < min_requested_count) return 0;
@@ -249,7 +249,7 @@ public:
 
     size_t pop_and_reserve(T* buffer, size_t min_requested_count, size_t max_requested_count, size_t location_id = 0) {
 
-        auto& mb = m_mailboxes[location_id];
+        auto& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
 
         if (mb.queue.size() < min_requested_count) return 0;
@@ -264,11 +264,11 @@ public:
         return nitems;
     }
 
-    size_t reserve(size_t min_requested_count, size_t max_requested_count, size_t domain) {
+    size_t reserve(size_t min_requested_count, size_t max_requested_count, size_t location_id) {
 
-        LocalMailbox& mb = m_mailboxes[domain];
+        LocalQueue& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
-        size_t available_count = m_threshold - mb.queue.size() - mb.reserved_count;
+        size_t available_count = m_capacity - mb.queue.size() - mb.reserved_count;
         size_t count = std::min(available_count, max_requested_count);
         if (count < min_requested_count) {
             return 0;
@@ -279,7 +279,7 @@ public:
 
     void unreserve(size_t reserved_count, size_t location_id) {
 
-        LocalMailbox& mb = m_mailboxes[location_id];
+        LocalQueue& mb = m_queues[location_id];
         std::lock_guard<std::mutex> lock(mb.mutex);
         assert(reserved_count < mb.reserved_count);
         mb.reserved_count -= reserved_count;
