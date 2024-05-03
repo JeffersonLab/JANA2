cace1ad62c5f4958dfe31f43e0466f5ce5b38ba9
Nathan Brei <nbrei@jlab.org>
2024-05-03 12:28:33 -0400

    JService uses CallWithJExceptionWrapper

--- src/libraries/JANA/JService.cc
+++ src/libraries/JANA/JService.cc
@@ -8,34 +8,17 @@
 void JService::DoInit(JServiceLocator* sl) {
     std::lock_guard<std::mutex> lock(m_mutex);
     if (this->m_status != Status::Uninitialized) return; 
-    try {
-        for (auto* parameter : m_parameters) {
-            parameter->Configure(*(m_app->GetJParameterManager()), m_prefix);
-        }
-        for (auto* service : m_services) {
-            service->Init(GetApplication());  // TODO: This badly needs to be renamed. Maybe Fetch?
-        }
-        this->acquire_services(sl);
-        this->Init();
-        this->m_status = Status::Initialized;
+
+    for (auto* parameter : m_parameters) {
+        parameter->Configure(*(m_app->GetJParameterManager()), m_prefix);
     }
-    catch (JException& ex) {
-        ex.plugin_name = m_plugin_name;
-        ex.component_name = m_type_name;
-        throw ex;
+
+    for (auto* service : m_services) {
+        service->Init(GetApplication());  // TODO: This badly needs to be renamed. Maybe Fetch?
     }
-    catch (std::exception& e){
-        auto ex = JException("Exception in JService::DoInit(): %s", e.what());
-        ex.nested_exception = std::current_exception();
-        ex.plugin_name = m_plugin_name;
-        ex.component_name = m_type_name;
-        throw ex;
-    }
-    catch (...) {
-        auto ex = JException("Unknown exception in JService::DoInit()");
-        ex.nested_exception = std::current_exception();
-        ex.plugin_name = m_plugin_name;
-        ex.component_name = m_type_name;
-        throw ex;
-    }
+
+    CallWithJExceptionWrapper("JService::acquire_services", [&](){ this->acquire_services(sl); });
+    CallWithJExceptionWrapper("JService::Init", [&](){ this->Init(); });
+
+    this->m_status = Status::Initialized;
 }
