a06aba1c76b30019c3a8ba59babfa3f8c8f4a7f0
Raiqa Rasool <86972954+RaiqaRasool@users.noreply.github.com>
2024-07-29 14:12:36 -0400

    directory structure confirmed

---
+++ containers/Docker/scripts/jana_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export CC=\$(which gcc)
+export CXX=\$(which g++)
+export BUILD_SCRIPTS=/group/halld/Software/build_scripts
+export PROJECT_ROOT=/workspace
+export JANA_HOME=\$PROJECT_ROOT/JANA2
+export JANA_PLUGIN_PATH=\$PROJECT_ROOT/JANA2/plugins
+source \$BUILD_SCRIPTS/gluex_env_boot_jlab.sh --bs \$BUILD_SCRIPTS
+gxenv \$PROJECT_ROOT/JANA2/jana_prereqs_version.xml
+echo "jana_home value: \$JANA_HOME"
+cd \$JANA_HOME
+mkdir -p build
+cd build
+echo "Building start"
+cmake3 \$JANA_HOME -DUSE_XERCES=1
+make -j8 install
