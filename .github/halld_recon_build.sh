515e73f215a3983dfa24a579f33a96920abfba52
Raiqa Rasool <86972954+RaiqaRasool@users.noreply.github.com>
2024-07-30 12:56:10 -0400

    make all bin jana files executable

--- .github/halld_recon_build.sh
+++ .github/halld_recon_build.sh
@@ -7,7 +7,7 @@ export BUILD_SCRIPTS=/group/halld/Software/build_scripts
 source $BUILD_SCRIPTS/gluex_env_boot_jlab.sh --bs $BUILD_SCRIPTS
 gxenv /workspace/JANA2/.github/halld_recon_build_prereqs_version.xml
 
-chmod +x $JANA_HOME/bin/jana-config
+chmod +x $JANA_HOME/bin/*
 
 cd src
 nice scons install
