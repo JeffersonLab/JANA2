04943301970052eaf07cbddfd658e4c2474e7177
Raiqa Rasool <86972954+RaiqaRasool@users.noreply.github.com>
2024-08-07 10:22:08 -0400

    check

--- .github/jana_build.sh
+++ .github/jana_build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "mounting cvmfs"
+yum -y install fuse
+chmod 666 /dev/fuse
+mkdir -p /cvmfs/oasis.opensciencegrid.org
+mount -t cvmfs oasis.opensciencegrid.org /cvmfs/oasis.opensciencegrid.org
 
 export CC=$(which gcc)
 export CXX=$(which g++)
-export BUILD_SCRIPTS=/group/halld/Software/build_scripts
+export BUILD_SCRIPTS=/cvmfs/oasis.opensciencegrid.org/gluex/group/halld/Software/build_scripts
 export PROJECT_ROOT=/workspace
 export JANA_HOME=$PROJECT_ROOT/JANA2
 export JANA_PLUGIN_PATH=$PROJECT_ROOT/JANA2/plugins
@@ -18,4 +23,4 @@ cd build
 
 echo "Building start"
 cmake3 $JANA_HOME -DUSE_XERCES=1 -DCMAKE_CXX_STANDARD=17
-make install
+make install -j12
