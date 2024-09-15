ad4eb315c16f1dafd1ed881300fccafbf792c5b1
Nathan Brei <nbrei@jlab.org>
2024-09-15 10:30:07 -0400

    Fix failing test case

--- src/libraries/JANA/JService.cc
+++ src/libraries/JANA/JService.cc
@@ -9,17 +9,19 @@ void JService::DoInit(JServiceLocator* sl) {
     std::lock_guard<std::mutex> lock(m_mutex);
     if (this->m_status != Status::Uninitialized) return; 
 
-    auto params = m_app->GetJParameterManager();
-    m_logger = params->GetLogger(GetLoggerName());
+    if (m_app != nullptr) {
 
-    for (auto* parameter : m_parameters) {
-        parameter->Configure(*params, m_prefix);
+        auto params = m_app->GetJParameterManager();
+        m_logger = params->GetLogger(GetLoggerName());
+
+        for (auto* parameter : m_parameters) {
+            parameter->Configure(*params, m_prefix);
+        }
+
+        for (auto* service : m_services) {
+            service->Fetch(m_app);
+        }
     }
-
-    for (auto* service : m_services) {
-        service->Fetch(m_app);
-    }
-
 
     CallWithJExceptionWrapper("JService::acquire_services", [&](){ this->acquire_services(sl); });
     CallWithJExceptionWrapper("JService::Init", [&](){ this->Init(); });
