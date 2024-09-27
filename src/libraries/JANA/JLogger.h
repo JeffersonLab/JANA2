1499a1c3aee2a6281d6f010e8142b168bacc24ae
Nathan Brei <nbrei@jlab.org>
2024-09-26 21:04:29 -0400

    Add timestamp to logger output

--- src/libraries/JANA/JLogger.h
+++ src/libraries/JANA/JLogger.h
@@ -4,9 +4,15 @@
 
 #pragma once
 
+#include <JANA/Compatibility/JStreamLog.h>
+
+#include <atomic>
 #include <iostream>
 #include <sstream>
-#include <JANA/Compatibility/JStreamLog.h>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+
 
 
 struct JLogger {
@@ -16,7 +22,7 @@ struct JLogger {
     std::string className;
     bool show_level = true;
     bool show_classname = false;
-    bool show_timestamp = false;
+    bool show_timestamp = true;
     bool show_threadstamp = false;
 
     explicit JLogger(JLogger::Level level = JLogger::Level::INFO,
@@ -39,13 +45,13 @@ static JLogger default_cerr_logger = JLogger(JLogger::Level::TRACE, &std::cerr, 
 
 inline std::ostream& operator<<(std::ostream& s, JLogger::Level l) {
     switch (l) {
-        case JLogger::Level::TRACE: return s << "TRACE";
-        case JLogger::Level::DEBUG: return s << "DEBUG";
-        case JLogger::Level::INFO:  return s << "INFO";
-        case JLogger::Level::WARN:  return s << "WARN";
-        case JLogger::Level::ERROR: return s << "ERROR";
-        case JLogger::Level::FATAL: return s << "FATAL";
-        default:               return s << "OFF";
+        case JLogger::Level::TRACE: return s << "trace";
+        case JLogger::Level::DEBUG: return s << "debug";
+        case JLogger::Level::INFO:  return s << "info";
+        case JLogger::Level::WARN:  return s << "warn ";
+        case JLogger::Level::ERROR: return s << "error";
+        case JLogger::Level::FATAL: return s << "fatal";
+        default:               return s << "off";
     }
 }
 
@@ -64,13 +70,35 @@ struct JLogMessage {
                 JLogger::Level level = JLogger::Level::INFO)
                 : logger(logger), level(level) {
 
+        if (logger.show_timestamp) {
+            auto now = std::chrono::system_clock::now();
+            std::time_t current_time = std::chrono::system_clock::to_time_t(now);
+            std::tm* local_time = std::localtime(&current_time);
+            char buffer[100];
+            std::strftime(buffer, sizeof(buffer), "%H:%M:%S", local_time);
+
+            // Extract milliseconds by calculating the duration since the last whole second
+            auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+            builder << buffer << ".";
+            builder << std::setfill('0') << std::setw(5) << milliseconds << std::setfill(' ') << " ";
+        }
         if (logger.show_level) {
-            builder << "[" << level << "] ";
+            switch (level) {
+                case JLogger::Level::TRACE: builder << "[trace] "; break;
+                case JLogger::Level::DEBUG: builder << "[debug] "; break;
+                case JLogger::Level::INFO:  builder << "[info]  "; break;
+                case JLogger::Level::WARN:  builder << "[warn]  "; break;
+                case JLogger::Level::ERROR: builder << "[error] "; break;
+                case JLogger::Level::FATAL: builder << "[fatal] "; break;
+                default: builder << "[?????] ";
+            }
+        }
+        if (logger.show_threadstamp) {
+            builder << "{" << std::this_thread::get_id() << "} ";
         }
         if (logger.show_classname) {
             builder << logger.className << ": ";
         }
-        // TODO: Re-add thread and timestamp info?
     }
 
     // Helper function for truncating long strings to keep our log readable
