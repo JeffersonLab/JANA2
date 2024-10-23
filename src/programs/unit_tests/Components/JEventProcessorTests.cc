e37175ff70649b6d972ca510d29a4b20c72ed770
Nathan Brei <nbrei@jlab.org>
2024-10-22 22:08:24 -0400

    Test callback sequence for JEventProcessors

--- src/programs/unit_tests/Components/JEventProcessorTests.cc
+++ src/programs/unit_tests/Components/JEventProcessorTests.cc
@@ -4,6 +4,9 @@
 #include <JANA/JEventProcessor.h>
 #include <JANA/JEventSource.h>
 
+namespace jana::jeventprocessortests {
+
+
 struct MyEventProcessor : public JEventProcessor {
     int init_count = 0;
     int process_count = 0;
@@ -85,3 +88,110 @@ TEST_CASE("JEventProcessor_Exception") {
     REQUIRE(found_throw == true);
 
 }
+
+
+struct SourceWithRunNumberChange : public JEventSource {
+    SourceWithRunNumberChange() {
+        SetCallbackStyle(CallbackStyle::ExpertMode);
+    }
+    Result Emit(JEvent& event) {
+        if (GetEmittedEventCount() < 2) {
+            event.SetRunNumber(48);
+        }
+        else {
+            event.SetRunNumber(49);
+        }
+        return Result::Success;
+    }
+};
+
+
+struct ProcWithExceptionsAndLogging : public JEventProcessor {
+    Parameter<bool> except_on_init {this, "except_on_init", false, "Except on init"};
+    Parameter<bool> except_on_beginrun {this, "except_on_beginrun", false, "Except on beginrun"};
+    Parameter<bool> except_on_process {this, "except_on_process", false, "Except on process"};
+    Parameter<bool> except_on_endrun {this, "except_on_endrun", false, "Except on endrun"};
+    Parameter<bool> except_on_finish {this, "except_on_finish", false, "Except on finish"};
+
+    std::vector<std::string> log;
+
+    void Init() override {
+        LOG_INFO(GetLogger()) << "ProcWithExceptionsAndLogging::Init" << LOG_END;
+        log.push_back("init");
+        if (*except_on_init) throw std::runtime_error("Mystery");
+    }
+    void BeginRun(const std::shared_ptr<const JEvent>&) override {
+        LOG_INFO(GetLogger()) << "ProcWithExceptionsAndLogging::BeginRun" << LOG_END;
+        log.push_back("beginrun");
+        if (*except_on_beginrun) throw std::runtime_error("Mystery");
+    }
+    void Process(const std::shared_ptr<const JEvent>&) override {
+        LOG_INFO(GetLogger()) << "ProcWithExceptionsAndLogging::Process" << LOG_END;
+        log.push_back("process");
+        if (*except_on_process) throw std::runtime_error("Mystery");
+    }
+    void EndRun() override {
+        LOG_INFO(GetLogger()) << "ProcWithExceptionsAndLogging::EndRun" << LOG_END;
+        log.push_back("endrun");
+        if (*except_on_endrun) throw std::runtime_error("Mystery");
+    }
+    void Finish() override {
+        LOG_INFO(GetLogger()) << "ProcWithExceptionsAndLogging::Finish" << LOG_END;
+        log.push_back("finish");
+        if (*except_on_finish) throw std::runtime_error("Mystery");
+    }
+};
+
+TEST_CASE("JEventProcessor_CallbackSequence") {
+    JApplication app;
+    auto sut = new ProcWithExceptionsAndLogging;
+    app.Add(sut);
+    // JApplication takes ownership of sut, so our pointer will become invalid when JApplication is destroyed
+
+    SECTION("NoRunNumber") {
+        app.Add(new JEventSource);
+        app.SetParameterValue("jana:nevents", 2);
+        app.Run();
+        REQUIRE(sut->log.size() == 6);
+        REQUIRE(sut->log.at(0) == "init");
+        REQUIRE(sut->log.at(1) == "beginrun");
+        REQUIRE(sut->log.at(2) == "process");
+        REQUIRE(sut->log.at(3) == "process");
+        REQUIRE(sut->log.at(4) == "endrun");
+        REQUIRE(sut->log.at(5) == "finish");
+    }
+    SECTION("ConstantRunNumber") {
+        app.Add(new SourceWithRunNumberChange);
+        app.SetParameterValue("jana:nevents", 2);
+        app.Run();
+        REQUIRE(sut->log.size() == 6);
+        REQUIRE(sut->log.at(0) == "init");
+        REQUIRE(sut->log.at(1) == "beginrun");
+        REQUIRE(sut->log.at(2) == "process");
+        REQUIRE(sut->log.at(3) == "process");
+        REQUIRE(sut->log.at(4) == "endrun");
+        REQUIRE(sut->log.at(5) == "finish");
+    }
+    SECTION("MultipleRunNumbers") {
+        app.Add(new SourceWithRunNumberChange);
+        app.SetParameterValue("jana:nevents", 5);
+        app.Run();
+        REQUIRE(sut->log.size() == 11);
+        REQUIRE(sut->log.at(0) == "init");
+        REQUIRE(sut->log.at(1) == "beginrun");
+        REQUIRE(sut->log.at(2) == "process");
+        REQUIRE(sut->log.at(3) == "process");
+        REQUIRE(sut->log.at(4) == "endrun");
+        REQUIRE(sut->log.at(5) == "beginrun");
+        REQUIRE(sut->log.at(6) == "process");
+        REQUIRE(sut->log.at(7) == "process");
+        REQUIRE(sut->log.at(8) == "process");
+        REQUIRE(sut->log.at(9) == "endrun");
+        REQUIRE(sut->log.at(10) == "finish");
+    }
+}
+
+
+
+
+} // namespace jana::jeventprocessortests
