abb5ba562a21c33717bd5696541a9ad183382190
Nathan Brei <nbrei@jlab.org>
2024-10-25 22:46:03 -0400

    JTriggeredArrow handles 'rejected' events correctly

--- src/libraries/JANA/Topology/JTriggeredArrow.h
+++ src/libraries/JANA/Topology/JTriggeredArrow.h
@@ -34,29 +34,38 @@ struct JTriggeredArrow : public JArrow {
             auto end_total_time = std::chrono::steady_clock::now();
 
             result.update(JArrowMetrics::Status::ComeBackLater, 0, 1, std::chrono::milliseconds(0), end_total_time - start_total_time);
+            return;
         }
-        else {
-            // Obtained the input we needed; arrow is ready to fire
-            // Remember that `input` might be nullptr, in case arrow doesn't need any input event
 
-            auto start_processing_time = std::chrono::steady_clock::now();
+        // Obtained the input we needed; arrow is ready to fire
+        // Remember that `input` might be nullptr, in case arrow doesn't need any input event
 
-            OutputData outputs;
-            size_t output_count;
-            JArrowMetrics::Status status = JArrowMetrics::Status::KeepGoing;
+        auto start_processing_time = std::chrono::steady_clock::now();
 
-            static_cast<DerivedT*>(this)->fire(input, outputs, output_count, status);
+        OutputData outputs;
+        size_t output_count;
+        JArrowMetrics::Status status = JArrowMetrics::Status::KeepGoing;
 
-            auto end_processing_time = std::chrono::steady_clock::now();
+        static_cast<DerivedT*>(this)->fire(input, outputs, output_count, status);
 
-            push(outputs, output_count, location_id);
+        auto end_processing_time = std::chrono::steady_clock::now();
 
-            auto end_total_time = std::chrono::steady_clock::now();
-
-            auto latency = (end_processing_time - start_processing_time);
-            auto overhead = (end_total_time - start_total_time) - latency;
-            result.update(status, 1, 1, latency, overhead);
+        // Even though fire() returns an output count, some of those 'outputs' might have been sent right back to 
+        // their input pool. These mustn't be counted as "processed" in the metrics.
+        size_t processed_count = 0;
+        for (size_t output=0; output<output_count; ++output) {
+            if (!m_places[outputs[output].second]->is_input) {
+                processed_count++;
+            }
         }
+
+        push(outputs, output_count, location_id);
+
+        auto end_total_time = std::chrono::steady_clock::now();
+
+        auto latency = (end_processing_time - start_processing_time);
+        auto overhead = (end_total_time - start_total_time) - latency;
+        result.update(status, processed_count, 1, latency, overhead);
     }
 };
 
