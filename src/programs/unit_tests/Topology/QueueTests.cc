ffbb90b1eefb4e75425f98dfc892d938b0f1deba
Nathan Brei <nbrei@jlab.org>
2024-11-08 00:33:28 -0500

    Streamline JEventQueue

--- src/programs/unit_tests/Topology/QueueTests.cc
+++ src/programs/unit_tests/Topology/QueueTests.cc
@@ -3,6 +3,7 @@
 // Subject to the terms in the LICENSE file found in the top-level directory.
 
 #include <JANA/Topology/JMailbox.h>
+#include <JANA/Topology/JEventQueue.h>
 
 #include "catch.hpp"
 
@@ -35,3 +36,44 @@ TEST_CASE("QueueTests_Basic") {
     REQUIRE(count == 2);
     REQUIRE(q.size() == 1);
 }
+
+TEST_CASE("JEventQueueTests_Basic") {
+
+    JEventQueue sut(2,1);
+
+    JEvent event1;
+    event1.SetEventNumber(1);
+
+    JEvent event2;
+    event2.SetEventNumber(2);
+
+    JEvent event3;
+    event3.SetEventNumber(3);
+
+    REQUIRE(sut.GetCapacity() == 2);
+    REQUIRE(sut.GetSize(0) == 0);
+
+    sut.Push(&event1, 0);
+    REQUIRE(sut.GetSize(0) == 1);
+
+    JEvent* e = sut.Pop(0);
+    REQUIRE(sut.GetSize(0) == 0);
+
+    e = sut.Pop(0);
+    REQUIRE(e == nullptr);
+
+    sut.Push(&event3, 0);
+    sut.Push(&event2, 0);
+    REQUIRE(sut.GetSize(0) == 2);
+
+    e = sut.Pop(0);
+    REQUIRE(e->GetEventNumber() == 3);
+    REQUIRE(sut.GetSize(0) == 1);
+
+    sut.Push(&event1, 0);
+    REQUIRE_THROWS(sut.Push(&event1, 0));
+}
+
+
+
+
