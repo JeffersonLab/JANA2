8de949c081d33cb0173a2e7b39eb9934afc56d27
Nathan Brei <nbrei@jlab.org>
2024-11-17 00:34:53 -0500

    Test JOmniFactory/JService interaction

--- src/programs/unit_tests/Components/JServiceTests.cc
+++ src/programs/unit_tests/Components/JServiceTests.cc
@@ -2,6 +2,8 @@
 #include <catch.hpp>
 #include <JANA/JApplication.h>
 #include <JANA/JService.h>
+#include <JANA/Components/JOmniFactory.h>
+#include <JANA/Components/JOmniFactoryGeneratorT.h>
 
 namespace jana::jservicetests {
 
@@ -27,6 +29,47 @@ TEST_CASE("JServiceTests_ExceptionInInit") {
 
         auto sut = app.GetService<DummyService>();
         REQUIRE(1 == 0); // Definitely shouldn't be reachable
+    }
+    catch (JException& e) {
+        REQUIRE(e.GetMessage() == "Something goes wrong");
+        REQUIRE(e.type_name == "jana::jservicetests::DummyService");
+        REQUIRE(e.function_name == "JService::Init");
+    }
+}
+
+struct DummyData {int x;};
+
+struct DummyOmniFactory: public jana::components::JOmniFactory<DummyOmniFactory> {
+
+    Service<DummyService> m_svc {this};
+    Output<DummyData> m_output {this};
+
+    void Configure() {
+    }
+
+    void ChangeRun(int32_t /*run_nr*/) {
+    }
+
+    void Execute(int32_t /*run_nr*/, uint64_t /*evt_nr*/) {
+        m_output().push_back(new DummyData{22});
+    }
+};
+
+TEST_CASE("JServiceTests_ExceptionInInit_Issue381") {
+    JApplication app;
+    app.ProvideService(std::make_shared<DummyService>());
+
+    auto gen = new components::JOmniFactoryGeneratorT<DummyOmniFactory>();
+    gen->AddWiring("dummy", {}, {"data"});
+    app.Add(gen);
+
+    try {
+        app.Initialize();
+        REQUIRE(1 == 0); // Shouldn't be reachable
+        auto event = std::make_shared<JEvent>(&app);
+        auto data = event->Get<DummyData>("data");
+        REQUIRE(1 == 0); // Definitely shouldn't be reachable
+        REQUIRE(data.at(0)->x == 22);
     }
     catch (JException& e) {
         REQUIRE(e.GetMessage() == "Something goes wrong");
