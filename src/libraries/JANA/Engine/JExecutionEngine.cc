0e5d1d08307c389351e71f1cd6b242c22c0e85ea
Nathan Brei <nbrei@jlab.org>
2024-11-26 21:14:20 -0500

    Improve log messages for manual arrow firing

--- src/libraries/JANA/Engine/JExecutionEngine.cc
+++ src/libraries/JANA/Engine/JExecutionEngine.cc
@@ -679,12 +679,15 @@ JArrow::FireResult JExecutionEngine::Fire(size_t arrow_id, size_t location_id) {
 
     std::unique_lock<std::mutex> lock(m_mutex);
     JArrow* arrow = m_topology->arrows[arrow_id];
+    LOG_INFO(GetLogger()) << "Attempting to fire arrow with name=" << arrow->get_name() << ", index=" << arrow_id << LOG_END;
 
     ArrowState& arrow_state = m_arrow_states[arrow_id];
     if (arrow_state.status == ArrowState::Status::Finished) {
+        LOG_INFO(GetLogger()) << "Firing unsuccessful: Arrow status is Finished." << arrow_id << LOG_END;
         return JArrow::FireResult::Finished;
     }
     if (!arrow_state.is_parallel && arrow_state.active_tasks != 0) {
+        LOG_INFO(GetLogger()) << "Firing unsuccessful: Arrow is sequential and already has an active task." << arrow_id << LOG_END;
         return JArrow::FireResult::NotRunYet;
     }
     arrow_state.active_tasks += 1;
@@ -701,11 +704,31 @@ JArrow::FireResult JExecutionEngine::Fire(size_t arrow_id, size_t location_id) {
     JArrow::FireResult result = JArrow::FireResult::NotRunYet;
 
     if (event != nullptr || port == -1) {
+        if (event != nullptr) {
+            LOG_INFO(GetLogger()) << "Input event #" << event->GetEventNumber() << " from port " << port << LOG_END;
+        }
+        else {
+            LOG_INFO(GetLogger()) << "No input events." << LOG_END;
+        }
+        LOG_INFO(GetLogger()) << "Firing arrow." << LOG_END;
         arrow->fire(event, outputs, output_count, result);
+        LOG_INFO(GetLogger()) << "Fired arrow." << LOG_END;
+        if (output_count == 0) {
+            LOG_INFO(GetLogger()) << "No output events." << LOG_END;
+        }
+        else {
+            for (size_t i=0; i<output_count; ++i) {
+                LOG_INFO(GetLogger()) << "Output event #" << outputs.at(i).first->GetEventNumber() << " on port " << outputs.at(i).second << LOG_END;
+            }
+        }
+
         lock.lock();
         arrow->push(outputs, output_count, location_id);
         arrow_state.active_tasks -= 1;
         lock.unlock();
+    }
+    else {
+        LOG_INFO(GetLogger()) << "Firing unsuccessful: Arrow needs an input event on port " << port << " but the queue or pool is empty." << LOG_END;
     }
 
     return result;
