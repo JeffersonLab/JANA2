b426ae742cb7b34ebf96cbf76732bea76b046432
Nathan Brei <nbrei@jlab.org>
2025-01-02 11:50:10 -0500

    Deprecate CLI/JVersion.h header

--- src/libraries/JANA/CLI/JVersion.h
+++ src/libraries/JANA/CLI/JVersion.h
@@ -5,4 +5,8 @@
 
 #include <JANA/JVersion.h>
 
-
+namespace jana::cli::jversion {
+[[deprecated("Use JANA/JVersion.h instead")]]
+constexpr static int header_is_deprecated = 0;
+constexpr static int warn_about_header_deprecation = header_is_deprecated;
+}
