b14a652b58cfdd51310eb3686ffe7bcf640efc00
Nathan Brei <nbrei@jlab.org>
2025-03-18 10:58:14 -0400

    jana-plot-scaletests handles relative dirs

--- scripts/jana-plot-scaletests.py
+++ scripts/jana-plot-scaletests.py
@@ -14,7 +14,7 @@ import seaborn as sns
 # In order to run with latex typesetting, you need:
 # sudo dnf install texlive texlive-type1cm dvipng
 
-def make_plot(plot_spec):
+def make_plot(plot_spec, input_dir):
 
     matplotlib.use('Agg')
     # matplotlib.pyplot must be imported AFTER matplotlib.use(...) is called
@@ -39,7 +39,11 @@ def make_plot(plot_spec):
         dark_color = colors_dark[color_idx]
         color_idx += 1
 
-        nthreads,avg_rate,rms_rate = np.loadtxt(subplot['datafile'], skiprows=1, usecols=(0,1,2), unpack=True)
+        subdatafile = subplot['datafile']
+        if not os.path.isabs(subdatafile):
+            subdatafile = os.path.join(input_dir, subdatafile)
+
+        nthreads,avg_rate,rms_rate = np.loadtxt(subdatafile, skiprows=1, usecols=(0,1,2), unpack=True)
         minnthreads = nthreads.min()
         maxnthreads = nthreads.max()
 
@@ -52,7 +56,7 @@ def make_plot(plot_spec):
         tputs = np.minimum(par_bottleneck, seq_bottleneck)
 
         # Create plot using matplotlib
-        plt.plot(nthreads, tputs, linestyle="-", color=color)
+        plt.plot(nthreads, tputs, linestyle="-", linewidth=1, color=color)
         plt.errorbar(nthreads, avg_rate, rms_rate, 
                      linestyle='',
                      marker='o',
@@ -75,46 +79,34 @@ if __name__ == "__main__":
         print()
         print('Cannot find file: ' + input_filename)
         print('''
-
     Usage:
         jana-plot-scaletests.py TOMLFILE
 
-    This script can be used to plot the results of running the
-    built in JANA scaling test. This test will automatically
-    record the event processing rate as it cycles through changing
-    the number of processing threads JANA is using. This can be
-    done if using the "jana" executable by just giving it the
-    -b argument. If using a custom executable you will need to
-    replace the call to JApplication::Run() with:
+    This script plots multiple JANA scaling tests on the same axes. It assumes you
+    use JANA2's built-in performance benchmarking tool to generate the scaling
+    curves. The input argument is a TOML file with the following contents:
 
-    JBenchmarker benchmarker(app);
-    benchmarker.RunUntilFinished();
+        - title: string
+        - output_filename: string
+        - use_latex: bool
+        - test: list of dicts containing:
+            - key: string              # Text used for the legend
+            - datafile: string         # Relative path to a `rates.dat` file
+            - t_seq: int               # Largest avg latency among sequential arrows
+            - t_par: int               # Largest avg latency among parallel arrows
 
-    You can control several parameters of the test using these
-    JANA configuration parameters:
+    Note: The output file format can be controlled by changing the extension on the
+          output file name. The most useful outputs are `pdf` and `png`.
 
-    benchmark:nsamples     # Number of samples for each benchmark test
-    benchmark:minthreads   # Minimum number of threads for benchmark test
-    benchmark:maxthreads   # Maximum number of threads for benchmark test
-    benchmark:resultsdir   # Output directory name for benchmark test results
-    benchmark:threadstep   # Delta number of threads between each benchmark test
+    Note: If you enable use_latex, you'll need to install texlive and a couple extra things.
+          On my RPM-based distro, I needed `sudo dnf install texlive texlive-type1cm dvipng`
 
-    Run "jana -c -b" to see the default values for each of these.
-
-    To use this just go into the benchmark:resultsdir where the
-    rates.dat file is and run it. Note that you must have python
-    installed as well as the numpy and matplotlib python packages.
-
-    If you wish to just create the plot as a PNG file without opening
-    a window (convenient for machines where a graphical environment
-    isn't present), then give it the -nowin argument like this:
-
-        jana-plot-scaletest.py -nowin
         ''')
         sys.exit(0)
 
     spec = toml.load(input_filename)
-    plt = make_plot(spec)
+    input_dir = os.path.abspath(os.path.dirname(input_filename))
+    plt = make_plot(spec, input_dir)
     output_filename = 'rate_vs_nthreads.pdf'
     if 'output_filename' in spec:
         output_filename = spec['output_filename']
