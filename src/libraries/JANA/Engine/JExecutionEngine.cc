22b88b651211978754436a4fb3c6848c251cfda1
Nathan Brei <nbrei@jlab.org>
2025-04-23 23:24:16 -0400

    JExecutionEngine throws a meaningful error when no event source present
    Previously it threw an ArithmeticException deep inside JExecutionEngine.
    
    This addresses issue #437

--- src/libraries/JANA/Engine/JExecutionEngine.cc
+++ src/libraries/JANA/Engine/JExecutionEngine.cc
@@ -78,6 +78,9 @@ void JExecutionEngine::RunTopology() {
     if (m_runstatus == RunStatus::Finished) {
         throw JException("Cannot switch topology runstatus to Running because it is already Finished");
     }
+    if (m_arrow_states.size() == 0) {
+        throw JException("Cannot execute an empty topology! Hint: Have you provided an event source?");
+    }
 
     // Set start time and event count
     m_time_at_start = clock_t::now();
@@ -106,6 +109,12 @@ void JExecutionEngine::ScaleWorkers(size_t nthreads) {
     // an external thread team, should the need arise.
 
     std::unique_lock<std::mutex> lock(m_mutex);
+
+    if (nthreads != 0 && m_arrow_states.size() == 0) {
+        // We check that (nthreads != 0) because this gets called at shutdown even if the topology wasn't run
+        // Remember, we want JApplication::Initialize() to succeed and JMain to shut down cleanly even when the topology is empty
+        throw JException("Cannot execute an empty topology! Hint: Have you provided an event source?");
+    }
 
     auto prev_nthreads = m_worker_states.size();
 
