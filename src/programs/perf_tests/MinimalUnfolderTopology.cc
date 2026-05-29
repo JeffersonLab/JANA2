ebab6770c37073c086a30bd5c9b1c9489ca05553
Nathan Brei <nbrei@jlab.org>
2026-05-29 12:55:02 -0400

    Add MinimalUnfolderTopology perf test

---
+++ src/programs/perf_tests/MinimalUnfolderTopology.cc
@@ -0,0 +1,115 @@
+
+#include "JANA/Utils/JEventLevel.h"
+#include <catch.hpp>
+
+#include <JANA/JApplication.h>
+#include <JANA/JFactoryGenerator.h>
+#include <JANA/CLI/JBenchmarker.h>
+#include <JANA/JEventUnfolder.h>
+#include <JANA/JEventProcessor.h>
+#include <JANA/JEventSource.h>
+
+
+namespace jana::minimal_unfolder_topology {
+
+struct Data { size_t x; };
+
+struct BSrc : public JEventSource {
+
+    Output<Data> data_out {this};
+
+    BSrc() {
+        SetLevel(JEventLevel::Block);
+        SetCallbackStyle(CallbackStyle::ExpertMode);
+        data_out.SetShortName("1");
+    }
+    JEventSource::Result Emit(JEvent& block) override {
+        data_out().push_back(new Data {block.GetEventNumber()*3 });
+        return Result::Success;
+    };
+};
+
+struct BFac : public JFactory {
+    Input<Data> data_in {this};
+    Output<Data> data_out {this};
+
+    BFac() {
+        SetLevel(JEventLevel::Block);
+        data_in.SetDatabundleName("1");
+        data_out.SetShortName("2");
+    }
+    void Process(const JEvent&) override {
+        auto x = data_in().at(0)->x + 7;
+        data_out().push_back(new Data {x});
+    };
+};
+
+struct Unf : public JEventUnfolder {
+    Input<Data> parent_data_in {this};
+    Output<Data> child_data_out {this};
+
+    Unf() {
+        SetParentLevel(JEventLevel::Block);
+        SetChildLevel(JEventLevel::PhysicsEvent);
+        SetCallbackStyle(CallbackStyle::ExpertMode);
+        parent_data_in.SetDatabundleName("2");
+        child_data_out.SetShortName("3");
+    }
+    JEventUnfolder::Result Unfold(const JEvent&, JEvent&, int child_nr) override {
+        auto x = parent_data_in().at(0)->x * 10 + child_nr;
+        child_data_out().push_back(new Data{x});
+
+        if (child_nr == 9) {
+            return Result::NextChildNextParent;
+        }
+        return Result::NextChildKeepParent;
+    };
+};
+
+struct PEFac : public JFactory {
+    Input<Data> data_in {this};
+    Output<Data> data_out {this};
+    PEFac() {
+        SetLevel(JEventLevel::PhysicsEvent);
+        data_in.SetDatabundleName("3");
+        data_out.SetShortName("4");
+    }
+    void Process(const JEvent&) override {
+        auto x = data_in().at(0)->x * 10;
+        data_out().push_back(new Data {x});
+    };
+};
+
+struct PEProc : public JEventProcessor {
+    Input<Data> data_in {this};
+    PEProc() {
+        SetLevel(JEventLevel::PhysicsEvent);
+        SetCallbackStyle(CallbackStyle::ExpertMode);
+        data_in.SetDatabundleName("4");
+    }
+    void ProcessSequential(const JEvent&) override {
+        (void) (data_in().at(0)->x);
+    };
+};
+
+
+TEST_CASE("MinimalUnfolderTopology") {
+
+    LOG << "Running MinimalUnfolderTopology";
+
+    JApplication app;
+    app.Add(new BSrc);
+    app.Add(new Unf);
+    app.Add(new PEProc);
+    app.Add(new JFactoryGeneratorT<BFac>);
+    app.Add(new JFactoryGeneratorT<PEFac>);
+
+    JBenchmarker benchmarker(&app);
+    benchmarker.RunUntilFinished();
+}
+
+
+}
+
+
+
