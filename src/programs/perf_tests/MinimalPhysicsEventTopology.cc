d75c6cacdaa80202bcf29f11e4907effd420c69d
Nathan Brei <nbrei@jlab.org>
2026-06-12 13:36:36 -0400

    Move mini physics event topology perf test off of JTest

--- src/programs/perf_tests/MinimalPhysicsEventTopology.cc
+++ src/programs/perf_tests/MinimalPhysicsEventTopology.cc
@@ -8,45 +8,96 @@
 #include <JANA/JApplication.h>
 #include <JANA/JFactoryGenerator.h>
 #include <JANA/CLI/JBenchmarker.h>
+#include <JANA/JEventUnfolder.h>
+#include <JANA/JEventProcessor.h>
+#include <JANA/JEventSource.h>
+
+
+namespace jana::minimal_physics_event_topology {
+
+struct Data { size_t x; };
+
+struct PESrc : public JEventSource {
+
+    Output<Data> data_out {this};
+
+    PESrc() {
+        SetCallbackStyle(CallbackStyle::ExpertMode);
+        data_out.SetShortName("1");
+    }
+    JEventSource::Result Emit(JEvent& event) override {
+        data_out().push_back(new Data {event.GetEventNumber()*3 });
+        return Result::Success;
+    };
+};
+
+struct PEFac : public JFactory {
+    Input<Data> data_in {this};
+    Output<Data> data_out {this};
+    PEFac() {
+        SetLevel(JEventLevel::PhysicsEvent);
+        data_in.SetDatabundleName("1");
+        data_out.SetShortName("2");
+    }
+    void Process(const JEvent&) override {
+        auto x = data_in().at(0)->x * 10;
+        data_out().push_back(new Data {x});
+    };
+};
+
+struct PEProc : public JEventProcessor {
+    Input<Data> data_in {this};
+    PEProc() {
+        SetLevel(JEventLevel::PhysicsEvent);
+        SetCallbackStyle(CallbackStyle::ExpertMode);
+        data_in.SetDatabundleName("2");
+    }
+    void ProcessSequential(const JEvent&) override {
+        (void) (data_in().at(0)->x);
+    };
+};
 
 
 TEST_CASE("MinimalPhysicsEventTopology") {
 
+    LOG << "Running MinimalPhysicsEventTopology";
+
     JApplication app;
+    app.SetParameterValue("nthreads", "1");
+    app.SetParameterValue("jana:nevents", "10000");
+    app.SetParameterValue("jana:backoff_interval", "1");
+    //app.SetParameterValue("jana:backoff_interval", "10");
+    //app.SetParameterValue("jana:loglevel", "trace");
 
-    app.SetParameterValue("jtest:parser:cputime_ms", 0);
-    app.SetParameterValue("jtest:parser:cputime_spread", 0);
-    app.SetParameterValue("jtest:parser:bytes", 0);
-    app.SetParameterValue("jtest:parser:bytes_spread", 0);
+    app.Add(new PESrc);
+    app.Add(new PEProc);
+    app.Add(new JFactoryGeneratorT<PEFac>);
+    app.Run();
+}
 
-    app.SetParameterValue("jtest:disentangler:cputime_ms", 0);
-    app.SetParameterValue("jtest:disentangler:cputime_spread", 0);
-    app.SetParameterValue("jtest:disentangler:bytes", 0);
-    app.SetParameterValue("jtest:disentangler:bytes_spread", 0);
+TEST_CASE("MinimalPhysicsEventTopology_Benchmarking") {
 
-    app.SetParameterValue("jtest:tracker:cputime_ms", 0);
-    app.SetParameterValue("jtest:tracker:cputime_spread", 0);
-    app.SetParameterValue("jtest:tracker:bytes", 0);
-    app.SetParameterValue("jtest:tracker:bytes_spread", 0);
+    LOG << "Running MinimalPhysicsEventTopology_Benchmarking";
 
-    app.SetParameterValue("jtest:plotter:cputime_ms", 0);
-    app.SetParameterValue("jtest:plotter:cputime_spread", 0);
-    app.SetParameterValue("jtest:plotter:bytes", 0);
-    app.SetParameterValue("jtest:plotter:bytes_spread", 0);
-
+    JApplication app;
     app.SetParameterValue("benchmark:resultsdir", "perf_tests");
     app.SetParameterValue("benchmark:rates_filename", "minimal_physics_event_topology.dat");
     app.SetParameterValue("benchmark:use_log_scale", true);
     app.SetParameterValue("benchmark:minthreads", "1");
     app.SetParameterValue("benchmark:maxthreads", "32");
-    //params->SetParameter("jana:loglevel", "off");
+    //app.SetParameterValue("jana:backoff_interval", "1");
+    app.SetParameterValue("jana:backoff_interval", "1");
+    //app.SetParameterValue("jana:loglevel", "trace");
 
-    auto logger = app.GetJParameterManager()->GetLogger("PerfTests");
-    app.AddPlugin("JTest");
+    app.Add(new PESrc);
+    app.Add(new PEProc);
+    app.Add(new JFactoryGeneratorT<PEFac>);
 
-    LOG_INFO(logger) << "Running JTest with all sleeps and computations turned off" << LOG_END;
     JBenchmarker benchmarker(&app);
     benchmarker.RunUntilFinished();
 }
+
+
+} // namespace
 
 
